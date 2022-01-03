d3986951c61d7b6a98e9a75001553382f868ac33
Sandy Cheng <cheek.codes@gmail.com>
2022-01-02 22:08:25 -0500

    two

--- 101_dalmations.js
+++ 101_dalmations.js
@@ -37,3 +37,10 @@ function howManyDalmatians(number){
       return "Woah that's a lot of dogs!"
     }
 }
+
+
+
+
+const dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+const howManyDalmatians = number  => number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number == 101 ? dogs[3] : dogs[2]));
