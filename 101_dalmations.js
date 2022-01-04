710d92a2bce8b445715cd72ad361a44acfd6f6f6
Sandy Cheng <cheek.codes@gmail.com>
2022-01-03 22:07:35 -0500

    one

--- 101_dalmations.js
+++ 101_dalmations.js
@@ -19,3 +19,21 @@ function howManyDalmations(numer :
   
 return respond
 */
+
+
+
+
+function howManyDalmatians(number){
+    if (number <= 10){
+      return "Hardly any"
+    }
+    else if(number <= 50){
+      return "More than a handful!"
+    }
+    else if (number === 101){
+      return "101 DALMATIANS!!!"
+    }
+    else {
+      return "Woah that's a lot of dogs!"
+    }
+}
