1dcd8b51ecab5fab0cb8078f0d6e732bf96b29eb
Sandy Cheng <cheek.codes@gmail.com>
2022-01-06 21:31:27 -0500

    two

--- fix_your_code_before_the_garden_dies.js
+++ fix_your_code_before_the_garden_dies.js
@@ -31,3 +31,14 @@ function rainAmount(mm){
       return `You need to give your plant ${40-mm}mm of water`
     }
 }
+
+
+
+function rainAmount(mm){
+    if (mm < 40) {
+         return "You need to give your plant " + (40-mm) + "mm of water"
+    }
+    else {
+         return "Your plant has had more than enough water for today!"
+    }
+}
