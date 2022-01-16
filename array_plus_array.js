ed722a786ae9a30d7a14b70257ac5467e52f3d87
Sandy Cheng <cheek.codes@gmail.com>
2022-01-15 20:56:56 -0500

    seven

--- array_plus_array.js
+++ array_plus_array.js
@@ -69,3 +69,9 @@ const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((a, b) => a + b
 function arrayPlusArray(arr1, arr2){
     return arr1.reduce((sum, cur) => sum + cur) + arr2.reduce((sum, cur) => sum + cur)
 }
+
+
+
+function arrayPlusArray(arr1, arr2){
+    return[...arr1, ...arr2].reduce((a, b) => a + b, 0)
+}
