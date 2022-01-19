e955216ae7df947a9e6fd31739e0322ad19cdc24
Sandy Cheng <cheek.codes@gmail.com>
2022-01-19 15:01:46 -0500

    seven

--- get_planet_name.js
+++ get_planet_name.js
@@ -147,3 +147,20 @@ function getPlanetName(i){
 
 
 const getPlanetName = id => ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'][id-1];
+
+
+
+
+function getPlanetName(id){
+    var name;
+   if(id === 1) name = 'Mercury';
+   if(id === 2) name = 'Venus';
+   if(id === 3) name = 'Earth';
+   if(id === 4) name = 'Mars';
+   if(id === 5) name = 'Jupiter';
+   if(id === 6) name = 'Saturn';
+   if(id === 7) name = 'Uranus';
+   if(id === 8) name = 'Neptune';
+    return name;
+  }
+  
