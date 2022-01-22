5d22f5e048830ce2ea0d0274ce236ebe59a04fe8
Sandy Cheng <cheek.codes@gmail.com>
2022-01-22 14:41:55 -0500

    one

--- unfinished_loop_debug.js
+++ unfinished_loop_debug.js
@@ -23,3 +23,13 @@
 
 
 // E: none
+
+
+
+function createArray(number){
+    var newArray = [];
+    for(var counter = 1; counter <= number; counter ++){
+      newArray.push(counter);
+    }
+    return newArray;
+}
