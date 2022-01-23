bc6ab1d9f32cf2eb8e4f94b360d2714ece88f5af
Sandy Cheng <cheek.codes@gmail.com>
2022-01-23 14:42:36 -0500

    three

--- unfinished_loop_debug.js
+++ unfinished_loop_debug.js
@@ -37,3 +37,9 @@ function createArray(number){
 
 
 const createArray = n => Array(...Array(n)).map((v, i) => i + 1);
+
+
+
+function createArray(number){
+    return Array.from({length:number},(v,i)=>i+1)
+}
