36eab7157e0cf33c3f496c04589365e59d17b8c7
Sandy Cheng <cheek.codes@gmail.com>
2022-01-27 14:44:32 -0500

    prompts

---
+++ fake_binary.js
@@ -0,0 +1,13 @@
+// Fake binary
+
+// Given a string of digits, you should replace any digit below 5 with0 and any digit 5 and above with 1. Return the resulting string.
+
+// Not: input will never be an empty string
+
+
+
+// P: Will we be given floats? Will we be given integers? Will we be given special characters? Will we be given strings? Will we be given an empty string?
+
+
+
+// R: Return a new string where if a digit is less than 5, it will be replaced with 0, while if a dig is greater than or equal to 5, it will be replaced with 1
