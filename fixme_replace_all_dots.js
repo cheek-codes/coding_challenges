003894436601ad652bc4b8c82d55d215a1532b90
Sandy Cheng <cheek.codes@gmail.com>
2022-01-29 14:17:29 -0500

    one with psuedocode

--- fixme_replace_all_dots.js
+++ fixme_replace_all_dots.js
@@ -34,5 +34,13 @@ var replaceDots = function(str) {
 
 
 var replaceDots = function(str) {
+    // added the \ to escape special characters
+    // added the g so that replace is run for all occurences in the string
     return str.replace(/\./g, '-');
+}
+
+
+
+var replaceDots = function(str) {
+    return str.replace(/[.]/g, '-');
 }
