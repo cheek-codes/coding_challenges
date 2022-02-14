52c0fe0e81e6991741981f66ca0079cee283435a
Sandy Cheng <cheek.codes@gmail.com>
2022-02-14 12:31:14 -0500

    fix bug

--- grasshopper_If_else_debug.js
+++ grasshopper_If_else_debug.js
@@ -6,6 +6,18 @@
 
 // The function receives one parameter health which will always be a whole number between -10 and 10.
 
+/*
+function checkAlive (health) {
+  if () {
+    health < 0
+    return false
+  } else () {
+    return true
+  }
+}
+*/
+
+
 
 
 // P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
@@ -21,3 +33,16 @@
 // E: 2. If we are given 0, should return false
 // E: 3. If we are given 2, should return true
 // E: 4. If we are given 9, should return true
+
+
+
+/*
+function checkAlive (health) {
+  if () {
+    health < 0
+    return false
+  } else () {
+    return true
+  }
+}
+*/
