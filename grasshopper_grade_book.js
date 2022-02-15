9481ab5ba3e0521eefcd17b8902f9c468c2fed74
Sandy Cheng <cheek.codes@gmail.com>
2022-02-15 15:24:36 -0500

    eight

--- grasshopper_grade_book.js
+++ grasshopper_grade_book.js
@@ -121,3 +121,14 @@ function getGrade (s1, s2, s3) {
 const getGrade = (s1, s2, s3) =>
   (val => val < 60 ? `F` : val < 70 ? `D` : val < 80 ? `C` : val < 90 ? `B` : `A`)
   ((s1 + s2 + s3) / 3);
+
+
+
+function getGrade (s1, s2, s3) {
+    let av = (s1 +s2 + s3) / 3;
+    return av >= 90 && av <= 100 ? 'A' : 
+           av >= 80 && av < 90 ? 'B' : 
+           av >= 70 && av < 80 ? 'C' :
+           av >= 60 && av < 70 ? 'D' :
+           'F'
+}
