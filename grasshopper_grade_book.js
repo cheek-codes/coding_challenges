87a3b090092e7cb5b766bc5bcdeccb24f6ef6218
Sandy Cheng <cheek.codes@gmail.com>
2022-02-15 15:25:02 -0500

    nine

--- grasshopper_grade_book.js
+++ grasshopper_grade_book.js
@@ -132,3 +132,10 @@ function getGrade (s1, s2, s3) {
            av >= 60 && av < 70 ? 'D' :
            'F'
 }
+
+
+
+function getGrade (s1, s2, s3) {
+    let score =  (s1+ s2+ s3) / 3;
+    return (score >= 90) ? 'A': (score >= 80) ? 'B': (score >= 70) ? 'C' : (score > 60)? 'D' : 'F';
+}
