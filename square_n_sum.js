c2da275ec160a96b7e9fede4d451169424e206cb
Sandy Cheng <cheek.codes@gmail.com>
2022-02-18 20:04:45 -0500

    return

--- square_n_sum.js
+++ square_n_sum.js
@@ -7,3 +7,11 @@
 
 
 // P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return a new integer where the given array has all the number squared and added together.
+
+
+
+
