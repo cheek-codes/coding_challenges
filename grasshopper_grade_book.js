30446a9b8e11d1acdeb57ec8e157c2683b7e88c9
Sandy Cheng <cheek.codes@gmail.com>
2022-02-20 14:49:43 -0500

    return

--- grasshopper_grade_book.js
+++ grasshopper_grade_book.js
@@ -14,3 +14,11 @@
 
 
 // P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers? Will we be given numbers above 100?
+
+
+
+// R: Return "A", if grades are greater than or equal to 90, but less than 100
+// R: Return "B", if grades are greater than or equal to 80, but less than 90
+// R: Return "C", if grades are greater than or equal to 70, but less than 80
+// R: Return "D", if grades are greater than or equal to 60, but less than 70
+// R: Return "F", if grades are greater than or equal to 0, but less than 60
