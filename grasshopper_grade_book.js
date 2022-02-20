6fd4f957557acc29119d657ae2435af87b687e91
Sandy Cheng <cheek.codes@gmail.com>
2022-02-20 14:47:35 -0500

    parameter

--- grasshopper_grade_book.js
+++ grasshopper_grade_book.js
@@ -10,3 +10,7 @@
 // 0 <= score < 60 --> 'F'
 
 // Tested values are all between 0 and 100. There is no need to check for negative values or values greater than 100.
+
+
+
+// P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers? Will we be given numbers above 100?
