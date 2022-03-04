6c086c407f506b23bd1685c1f4508661163ad3da
Sandy Cheng <cheek.codes@gmail.com>
2022-03-04 14:21:09 -0500

    return

--- grasshopper_debug.js
+++ grasshopper_debug.js
@@ -25,3 +25,7 @@
 
 
 // P: Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return a string that works. fix the bugs.
