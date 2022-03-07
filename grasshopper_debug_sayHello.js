5a35ebec341114d1933caecca654ba917751867f
Sandy Cheng <cheek.codes@gmail.com>
2022-03-07 13:37:43 -0500

    parameters

--- grasshopper_debug_sayHello.js
+++ grasshopper_debug_sayHello.js
@@ -4,3 +4,8 @@
 
 // Example output:
 // Hello, Mr. Spock
+
+
+
+
+// P: Will we be given floats? Will we be given integers? Will we be given special characters? Will we be given strings? Will we be given empty strings?
