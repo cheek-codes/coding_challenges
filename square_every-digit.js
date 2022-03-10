d9c8fdcb0ebeaaea50481f391371b5e5f6179eae
Sandy Cheng <cheek.codes@gmail.com>
2022-03-10 13:29:30 -0500

    seven

--- square_every-digit.js
+++ square_every-digit.js
@@ -78,3 +78,16 @@ function squareDigits(num){
     }
     return Number(results.join(''))
 }
+
+
+
+function squareDigits(num){
+    let result = num
+      .toString()
+      .split("")
+      .map( num => parseInt(num) )
+      .map( num => num * num )
+      .join("")
+     
+    return parseInt(result)
+}
