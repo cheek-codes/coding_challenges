b6e43dc5ef7c3b07925c5bb363372425cb66ffff
Sandy Cheng <cheek.codes@gmail.com>
2022-03-11 20:13:42 -0500

    example

--- square_n_sum.js
+++ square_n_sum.js
@@ -6,12 +6,20 @@
 
 
 
-// P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+// P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers? Will we be given an empty array?
 
 
 
 // R: Return a new integer where the given array has all the number squared and added together.
 
+
+
+// E: 1. If we were given [1, 2, 2], should return 9
+// E: 2. If we were given [1, 2], should return 5
+// E: 3. If we were given [0, 3, 4, 5], should return 50
+// E: 4. If we were given [], should return 0
+// E: 5. If we were given [-1, -2], should return 5
+// E: 6. If we were given [-1, 0, 1], should return 2
 
 
 
