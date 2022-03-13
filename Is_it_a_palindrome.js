fb8510b9d43ecf02d0105033546b0596789a6acc
Sandy Cheng <cheek.codes@gmail.com>
2022-03-12 23:56:39 -0400

    four

--- Is_it_a_palindrome.js
+++ Is_it_a_palindrome.js
@@ -46,3 +46,15 @@ const isPalindrome = x => x.toLowerCase().split('').reverse().join('') == x.toLo
 
 
 
+function isPalindrome(x) {
+    x = x.toLowerCase()
+    let a = 0
+    let b = x.length
+    while (a < b) {
+      if (x[a] != x[b-1])
+        return false
+      a++
+      b--
+    }
+    return true
+}
