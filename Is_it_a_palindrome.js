e7ad203ba8010b7f6c18feea1cf2c2ee6110a625
Sandy Cheng <cheek.codes@gmail.com>
2022-03-14 23:48:37 -0400

    one

--- Is_it_a_palindrome.js
+++ Is_it_a_palindrome.js
@@ -17,3 +17,21 @@
 // E: 2. If we are given "Abba", should return true
 // E: 3. If we are given "apple", should return false
 // E: 4. If we are given "snack", should return false
+
+
+
+function isPalindrome(x) {
+    // create a new variable where it will split, reverse, and rejoin x and change it to lower case so it will be case insensitive
+    let pal = x.split("").reverse().join("").toLowerCase()
+
+    // create a new variable to change x to lower case
+    let xx = x.toLowerCase()
+
+    // create an if else statement to check if pal and xx are palindromes. return true if yes. return false if no
+    if (pal === xx){
+      return true
+    }
+    else{
+      return false
+    } 
+}
