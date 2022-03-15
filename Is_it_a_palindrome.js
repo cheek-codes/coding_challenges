a2efc7eb507afb6a643ddce8dd72587c42aefccd
Sandy Cheng <cheek.codes@gmail.com>
2022-03-14 23:37:10 -0400

    example

--- Is_it_a_palindrome.js
+++ Is_it_a_palindrome.js
@@ -10,3 +10,10 @@
 
 // R: Return true, if the given is a palindrome, case insensitive
 // R: Return false, if the given is not a palindrome, case insensitive
+
+
+
+// E: 1. If we are given "abba", should return true
+// E: 2. If we are given "Abba", should return true
+// E: 3. If we are given "apple", should return false
+// E: 4. If we are given "snack", should return false
