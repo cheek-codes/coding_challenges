62c3d11a6e71ad092f2f8c1428cbf70d9ace88c1
Sandy Cheng <cheek.codes@gmail.com>
2022-03-15 23:31:10 -0400

    return

--- Is_it_a_palindrome.js
+++ Is_it_a_palindrome.js
@@ -5,3 +5,8 @@
 
 
 // P: Will we be given floats? Will we be given integers? Will we be given special characters? Will we be given strings? Will we be given empty strings?
+
+
+
+// R: Return true, if the given is a palindrome, case insensitive
+// R: Return false, if the given is not a palindrome, case insensitive
