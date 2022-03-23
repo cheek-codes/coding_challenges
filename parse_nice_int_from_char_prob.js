5b5c8ff5c6150acfec571f0055eda7c8f48eb249
Sandy Cheng <cheek.codes@gmail.com>
2022-03-22 21:37:36 -0400

    parameter

--- parse_nice_int_from_char_prob.js
+++ parse_nice_int_from_char_prob.js
@@ -5,3 +5,7 @@
 // Write a program that return sthe girl's age (0-9) as an integer.
 
 // Assume the test input string is always a valid string. For example, the test input may be "1 year old", or "5 years old". The first character in the string is always a number.
+
+
+
+// P: Will we be given a string? Will we be given an empty string? Will we be given  special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
