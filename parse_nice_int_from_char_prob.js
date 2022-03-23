354e4da07425ad898ca52b359d4d8ab9b7fd1c92
Sandy Cheng <cheek.codes@gmail.com>
2022-03-22 21:40:18 -0400

    return

--- parse_nice_int_from_char_prob.js
+++ parse_nice_int_from_char_prob.js
@@ -9,3 +9,7 @@
 
 
 // P: Will we be given a string? Will we be given an empty string? Will we be given  special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return the first character in the string as an integer
