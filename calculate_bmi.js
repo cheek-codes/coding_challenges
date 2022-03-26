d2b61fbfaf12227a80b58938db4eaf9d74fec1bb
Sandy Cheng <cheek.codes@gmail.com>
2022-03-25 21:21:46 -0400

    six

--- calculate_bmi.js
+++ calculate_bmi.js
@@ -98,3 +98,19 @@ function bmi(weight, height) {
       return 'Obese';
     }
 }
+
+
+
+function bmi(weight, height) {
+    let bmi = weight / (height * height);
+    switch(true){
+      case bmi <= 18.5:
+        return "Underweight";
+      case bmi <= 25.0:
+        return "Normal";
+      case bmi <= 30.0:
+        return "Overweight";
+      case bmi > 30:
+        return "Obese";
+     }
+}
