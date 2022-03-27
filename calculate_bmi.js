852ed1b0bb60e019a06960f72fedd388ca816153
Sandy Cheng <cheek.codes@gmail.com>
2022-03-26 21:20:54 -0400

    four

--- calculate_bmi.js
+++ calculate_bmi.js
@@ -71,3 +71,14 @@ const bmi = (w, h, bmi = w/h/h) =>
     bmi <= 18.5 ? "Underweight" :
     bmi <= 25 ? "Normal" :
     bmi <= 30 ? "Overweight" : "Obese";
+
+
+
+
+function bmi(weight, height) {
+
+    var bmi  = weight/(height*height);
+    
+        return bmi < 18.5 ? "Underweight" : bmi <=25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese";
+    
+}
