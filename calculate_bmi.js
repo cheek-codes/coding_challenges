183a186db4589f236dd6f3e72beee7b31b3ea1bd
Sandy Cheng <cheek.codes@gmail.com>
2022-03-29 15:31:49 -0400

    return

--- calculate_bmi.js
+++ calculate_bmi.js
@@ -13,3 +13,10 @@
 
 
 // P: Will we be given strings? Will we be given an empty string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return "Underweight" if bmi <= 18.5
+// R: Return "Normal" if bmi <= 25.0
+// R: Return "Overweight" if bmi <= 30.0
+// R: Return "Obese" if bmi > 30
