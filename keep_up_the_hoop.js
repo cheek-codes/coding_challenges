d0cb478e6174664418f0ab2b70b9a4c8eb69d7d6
Sandy Cheng <cheek.codes@gmail.com>
2022-04-02 15:06:51 -0400

    four

--- keep_up_the_hoop.js
+++ keep_up_the_hoop.js
@@ -44,3 +44,12 @@ const hoopCount = n => n>=10? "Great, now move on to tricks" : "Keep at it until
 function hoopCound (n) {
     return (n < 10) ? "Keep at it until you get it" : "Great, now move on to tricks"
 }
+
+
+
+function hoopCount (n) {
+    const HOPS_LIMIT = 10
+    return n >= HOPS_LIMIT
+      ? 'Great, now move on to tricks'
+      : 'Keep at it until you get it';
+}
