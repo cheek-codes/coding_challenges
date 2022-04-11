2a163393ea8bd4a1ca905ba10d44345f53247269
Sandy Cheng <cheek.codes@gmail.com>
2022-04-11 00:02:29 -0400

    parameter

--- multiply.js
+++ multiply.js
@@ -1,3 +1,7 @@
 // Multiply
 
 // This code does not execute properly. Try to figure out why.
+
+
+
+// P: Will we be given a string? Will we be given an empty string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
