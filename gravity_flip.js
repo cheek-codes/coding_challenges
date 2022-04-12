66f81df71b7cdf9ae0af3edcac04eb3df6327ea5
Sandy Cheng <cheek.codes@gmail.com>
2022-04-11 23:43:27 -0400

    return

--- gravity_flip.js
+++ gravity_flip.js
@@ -25,3 +25,8 @@
 
 
 // P: Will we be given a string? Will we be given an empty string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return a new string where if d was === "R", the order of the array would be in order from smallest to largest
+// R: Return a new string where if d was === "L", the order of the array would be in order from largest to smallest
