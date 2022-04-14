20f634a73fa5ebd74fd1dde26ee1a3e235642e70
Sandy Cheng <cheek.codes@gmail.com>
2022-04-13 23:51:52 -0400

    three

--- gravity_flip.js
+++ gravity_flip.js
@@ -50,3 +50,7 @@ const flip=(d, a)=>{
     if(d === 'R') return a.sort((a,b)=>a-b);
     if(d === 'L') return a.sort((a,b)=>b-a);
 }
+
+
+
+const flip = (d, a) => a.sort((x, y) => d === 'R' ? x - y : y - x);
