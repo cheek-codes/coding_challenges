2343c5c6c4908715bdf971acd42ccf8ac0888314
Sandy Cheng <cheek.codes@gmail.com>
2022-04-19 23:20:44 -0400

    parameter

--- if_you_can't_sleep,_just_count_sheep.js
+++ if_you_can't_sleep,_just_count_sheep.js
@@ -4,3 +4,7 @@
 
 // Task:
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+
+
+// P: Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
