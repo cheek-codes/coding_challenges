3be55376172ab6935f987db4a077575b06fa3907
Sandy Cheng <cheek.codes@gmail.com>
2022-04-20 23:22:01 -0400

    return

--- if_you_can't_sleep,_just_count_sheep.js
+++ if_you_can't_sleep,_just_count_sheep.js
@@ -8,3 +8,7 @@
 
 
 // P: Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return a new string where it will be counting the number of sheeps that was given
