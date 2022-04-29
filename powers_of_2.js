a153e35d647baf4d7dbc905c361a1fb47545a4c2
Sandy Cheng <cheek.codes@gmail.com>
2022-04-28 22:25:47 -0400

    parameter

--- powers_of_2.js
+++ powers_of_2.js
@@ -6,3 +6,7 @@
 // n = 0  ==> [1]        # [2^0]
 // n = 1  ==> [1, 2]     # [2^0, 2^1]
 // n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+
+
+// P: Will we be given strings? Will we be given an empty string? Will we be given an array? Will we be given an empty array? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
