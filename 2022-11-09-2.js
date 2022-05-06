201e86a4b8ad34c831fc999883bf3968b75d70c3
Sandy Cheng <cheek.codes@gmail.com>
2022-05-05 21:49:03 -0400

    parameter

--- 2022-11-09-2.js
+++ 2022-11-09-2.js
@@ -1,3 +1,10 @@
 // Function 2 -squaring an argument
 
 // Now you have to write a function that takes an argument and returns the square of it
+
+
+
+// P: will we be given a string? Will we be given an empty stirng? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
