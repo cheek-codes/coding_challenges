3af382a7b228814c189a5914d7b7f2c6d984b5b2
Sandy Cheng <cheek.codes@gmail.com>
2022-05-09 21:59:43 -0400

    prompt

---
+++ function_3_multiplying_two_nums.js
@@ -0,0 +1,6 @@
+// Function 3 - multiplying two numbers
+
+// Implement a function called multiply, which takes two numbers and returns their product:
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
