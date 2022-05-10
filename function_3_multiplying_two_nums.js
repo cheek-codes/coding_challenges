da8423a20a7e440df749595ef83dfef5e7596de8
Sandy Cheng <cheek.codes@gmail.com>
2022-05-09 22:00:51 -0400

    parameter

--- function_3_multiplying_two_nums.js
+++ function_3_multiplying_two_nums.js
@@ -4,3 +4,7 @@
 // multiply(2, 3) = 6
 // multiply(0, 188) = 0
 // multiply(85, 144) = 12240
+
+
+
+// P: Will we be given an empty string? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
