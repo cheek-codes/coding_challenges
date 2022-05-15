569d4fbd2e2a943bdfab2e628a4398ecbd2ea440
Sandy Cheng <cheek.codes@gmail.com>
2022-05-14 22:07:29 -0400

    two

--- check_same_case.js
+++ check_same_case.js
@@ -43,3 +43,17 @@ function sameCase(a, b){
        return 0
      }
 }
+
+
+
+function sameCase(a, b){
+    const alpha = 'abcdefghijklmnopqrstuvwxyz'
+    if(!alpha.includes(a.toLowerCase()) || !alpha.includes(b.toLowerCase())) return -1
+    
+    const checkCase = x => {
+      if(x === x.toLowerCase()) return 'lower'
+      if(x === x.toUpperCase()) return 'upper'
+    }
+  
+    return checkCase(a) === checkCase(b) ? 1 : 0
+}
