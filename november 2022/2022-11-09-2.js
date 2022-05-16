95823730926372da1de5715860c4b95f85367c84
Sandy Cheng <cheek.codes@gmail.com>
2022-05-15 21:56:39 -0400

    change location

---
+++ november 2022/2022-11-09-2.js
@@ -0,0 +1,46 @@
+// Function 2 -squaring an argument
+
+// Now you have to write a function that takes an argument and returns the square of it
+
+
+
+// P: will we be given a string? Will we be given an empty stirng? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return a square of the argument that was given
+
+
+
+// E: 1. If we were given 2, should return 4
+// E: 2. If we were given 4, should return 16
+// E: 3. If we were given 1, should return 1
+// E: 4. If we were given 10, should return 100
+
+
+
+function square(n){
+    // lets do some maths
+    return n ** 2
+}
+
+
+
+var square = function(a){
+    return a*a;
+}
+
+
+
+const square = (n) => n * n;
+
+
+
+function square(num){
+    var num = Math.pow(num, 2);
+    return num;
+};
+
+
+
+const square = x => x ** 2;
