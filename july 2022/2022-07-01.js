8475bfd9ddde1ea508ef3b8f7752c9d2f96a3463
Sandy Cheng <cheek.codes@gmail.com>
2022-05-16 21:39:56 -0400

    returns

--- july 2022/2022-07-01.js
+++ july 2022/2022-07-01.js
@@ -9,3 +9,7 @@
 
 
 // P: Will we be given empty strings? Will we be given strings? Will we be given floats? Will we be given integers? Will we be given special characters?
+
+
+
+// R:  Return a falsie
