01051c3f4051a68826840dad46957321e3f3a965
Sandy Cheng <cheek.codes@gmail.com>
2022-05-16 21:38:02 -0400

    paramters

--- july 2022/2022-07-01.js
+++ july 2022/2022-07-01.js
@@ -5,3 +5,7 @@
 // a == false
 // !a == false
 // a == !a
+
+
+
+// P: Will we be given empty strings? Will we be given strings? Will we be given floats? Will we be given integers? Will we be given special characters?
