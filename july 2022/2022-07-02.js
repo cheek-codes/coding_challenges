d9d8b8518d83d9980abb2dab9f254e1f20fcd215
Sandy Cheng <cheek.codes@gmail.com>
2022-05-19 21:32:26 -0400

    four

--- july 2022/2022-07-02.js
+++ july 2022/2022-07-02.js
@@ -84,3 +84,21 @@ const quarterOf = (month, quarter = 4) => {
     }
     return quarter;
   };
+
+
+
+  const quarterOf = (month) => {
+    if (month <= 3) {
+      quarter = 1; }
+    else if (month <=6) {
+      quarter = 2; }
+    else if (month <=9) {
+      quarter = 3; }
+    else if (month <=12) {
+      quarter = 4; }
+    else {
+      console.log('month is not valid')
+    }
+    
+    return quarter  
+}
