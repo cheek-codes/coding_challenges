b3809efa1f0f6207352b53860d722ceca2450273
Sandy Cheng <cheek.codes@gmail.com>
2022-05-20 21:30:55 -0400

    one

--- july 2022/2022-07-02.js
+++ july 2022/2022-07-02.js
@@ -29,3 +29,21 @@
 // E: 10. If we are given month 10, should return 4
 // E: 11. If we are given month 11, should return 4
 // E: 12. If we are given month 12, should return 4
+
+
+
+const quarterOf = (month) => {
+    //create an if else statement for each quarters
+    if (month === 1 || month === 2 || month === 3){
+        return 1
+    }
+    else if (month === 4 || month === 5 || month === 6){
+        return 2
+    }
+    else if (month === 7 || month === 8 || month === 9){
+        return 3
+    }
+    else{
+        return 4
+    }
+}
