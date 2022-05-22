c7e6e228d809c5b772a3727044ab78ecc9ffa049
Sandy Cheng <cheek.codes@gmail.com>
2022-05-21 21:25:11 -0400

    parameter

--- july 2022/2022-07-02.js
+++ july 2022/2022-07-02.js
@@ -3,3 +3,7 @@
 // Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
 // For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+
+
+// P: Will we be given an empty string? Will we be given a string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers/ Will we be given positive numbers?
