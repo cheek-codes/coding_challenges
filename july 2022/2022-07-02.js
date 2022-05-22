7a51232277a6ad25ee582268850122ec4bef328b
Sandy Cheng <cheek.codes@gmail.com>
2022-05-21 21:27:06 -0400

    return

--- july 2022/2022-07-02.js
+++ july 2022/2022-07-02.js
@@ -7,3 +7,10 @@
 
 
 // P: Will we be given an empty string? Will we be given a string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers/ Will we be given positive numbers?
+
+
+
+// R: Return 1 if the given month is Jan, Feb, or March
+// R: Return 2 if the given month is Apr, May, June
+// R: Return 3 if the given month is July, Aug, Sept
+// R: Return 4 if the given month is Oct, Nov, Dec
