7e4f5cb8f531084fe8783674991d6c107b8caa32
Sandy Cheng <cheek.codes@gmail.com>
2022-05-22 21:03:24 -0400

    parameter

--- square_n_sum.js
+++ square_n_sum.js
@@ -3,3 +3,7 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9
+
+
+
+// P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
