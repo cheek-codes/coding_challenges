7b66e549a550b652770e409c9edcc38c75abfe05
Sandy Cheng <cheek.codes@gmail.com>
2022-05-26 21:14:26 -0400

    parameter

--- july 2022/2022-07-03.js
+++ july 2022/2022-07-03.js
@@ -5,3 +5,7 @@
 // current father's age (years)
 // current age of his son (years)
 // Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+
+
+// P: Will we be given empty strings? Will we be given strings/ Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
