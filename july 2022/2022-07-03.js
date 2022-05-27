4d54ff4abdcf35a74168eae32253a7c249e6243b
Sandy Cheng <cheek.codes@gmail.com>
2022-05-26 21:15:35 -0400

    return

--- july 2022/2022-07-03.js
+++ july 2022/2022-07-03.js
@@ -9,3 +9,7 @@
 
 
 // P: Will we be given empty strings? Will we be given strings/ Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// Return an integer where the fater was twice as old as the son
