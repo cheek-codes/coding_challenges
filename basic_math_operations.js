7e0d3846cc2336685af375c580d850e7fd849195
Sandy Cheng <cheek.codes@gmail.com>
2022-05-27 22:11:04 -0400

    two

--- basic_math_operations.js
+++ basic_math_operations.js
@@ -39,3 +39,18 @@ function basicOp(operation, value1, value2){
       return (value1 / value2)
     }
 }
+
+
+
+function basicOp(operation, value1, value2){
+    switch(operation){
+        case "+":
+          return (value1 + value2)
+        case "-":
+          return (value1 - value2)
+        case "*":
+          return (value1 * value2)
+        case "/":
+          return (value1 / value2)
+    }
+}
