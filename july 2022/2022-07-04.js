a4e193fe0665ca678dc68c8ada2425cfb5382ae5
Sandy Cheng <cheek.codes@gmail.com>
2022-05-28 21:10:11 -0400

    four

--- july 2022/2022-07-04.js
+++ july 2022/2022-07-04.js
@@ -38,3 +38,13 @@ function reverseList(list){
 function reverseList(list) {
     return list.slice().reverse();
 }
+
+
+
+function reverseList(list) {
+    var array = [];
+    for (var i = list.length-1; i >= 0; i --){
+      array.push(list[i]);
+    }
+    return array;
+}
