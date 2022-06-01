37636387302833de48b08e78b9b07178d18bbef9
Sandy Cheng <cheek.codes@gmail.com>
2022-05-31 20:45:59 -0400

    parameter

--- november 2022/2022-11-09.js
+++ november 2022/2022-11-09.js
@@ -2,3 +2,10 @@
 
 // Code as fast as you can ! you need to double the integer and return it
 
+
+
+
+// P: Will I be given integers? Will I be given floats? Will I be given strings? Will I be given empty strings? Will I be given special characters?
+
+
+
