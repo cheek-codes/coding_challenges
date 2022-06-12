8eb0387aa2cae984886df43f34b223bcbdc95c79
Sandy Cheng <cheek.codes@gmail.com>
2022-06-11 23:00:50 -0400

    parameters

--- july 2022/2022-07-07.js
+++ july 2022/2022-07-07.js
@@ -3,3 +3,7 @@
 // We need a function that can transform a string into a number. What wyas of achieving this do you know?
 
 // Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+
+
+// P: Will we be given integers? Will we be given floats? Will we be given negative numbers? Will we be given positive numbers? Will we be given special characters? Will we be given an array? Will we be given a string? Will we be given an empty string?
