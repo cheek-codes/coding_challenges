8ff5ab972153ab1bf2ebbf920f10e149a64200e9
Sandy Cheng <cheek.codes@gmail.com>
2022-06-29 21:04:44 -0400

    parameter

--- july 2022/2022-07-04.js
+++ july 2022/2022-07-04.js
@@ -5,3 +5,7 @@
 // Examples (Input -> Output)
 // [1, 2, 3, 4]  -> [4, 3, 2, 1]
 // [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+
+
+// P: Will we be given empty strings? Will we be given strings/ Will we be given special characters? Will we be given floats? will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
