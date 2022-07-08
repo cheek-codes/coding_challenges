ecefaea967c68abb26528feac290a94cea16b9a9
Sandy Cheng <cheek.codes@gmail.com>
2022-07-07 22:59:21 -0400

    prompt

---
+++ july 2022/2022-07-07.js
@@ -0,0 +1,5 @@
+// Convert a string to a number!
+
+// We need a function that can transform a string into a number. What wyas of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
