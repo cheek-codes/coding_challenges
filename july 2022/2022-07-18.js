6096b8ccd1568536522987677859b7b052b7628a
Sandy Cheng <cheek.codes@gmail.com>
2022-07-18 21:56:54 -0400

    one

--- july 2022/2022-07-18.js
+++ july 2022/2022-07-18.js
@@ -20,3 +20,19 @@
 // E: 2. If we are given a number parameter of 5, and a string parameter of "Hello", should return "HelloHelloHelloHelloHello"
 // E: 3. If we are given a number parameter of 2, and a string parameter of "9", should return "99"
 // E: 4. If we are given a number parameter of 8, and a string parameter of "fuCK", should return "fuCKfuCKfuCKfuCKfuCKfuCKfuCKfuCK"
+
+
+
+function repeatStr(n, s) {
+    // create a new variable of str to hold new string
+    let str = ""
+
+    // create a for loop to loop through n so the program knows how many times to print s
+    for (let i=0; i<n; i++){
+        // add the newly printed s to the variable that you made called str
+        str += s
+    }
+
+    // return the variable str
+    return str
+}
