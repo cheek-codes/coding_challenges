979f8c0d7f3d55024afcc65b2186cae28e1ac31a
Sandy Cheng <cheek.codes@gmail.com>
2022-07-31 15:04:07 -0400

    second commit

---
+++ 2022-07-31/main.js
@@ -0,0 +1,9 @@
+// Hex to decimal
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// P: Will I be given special characters? Will there be an empty string? Will there be letters?
+
+function hexToDec(hexString){
+    //your code here
+    return parseInt(hexString, 16)
+  }
