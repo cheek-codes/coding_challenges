3946b091414582e35d3580200bc5c07d65a2bc54
Sandy Cheng <cheek.codes@gmail.com>
2022-08-08 07:09:33 -0400

    third

--- august 2022/2022-08-08.js
+++ august 2022/2022-08-08.js
@@ -16,21 +16,3 @@
 // Make sure you cover the cases where certain words do no show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
 
 
-// P: Will I be given special characters? Will there be an empty strings? Will it only be strings? Will there be numbers? 
-
-
-// R: Return a concatinated string using ES6, without [.concat()], [+], [.join()], and other methods
-
-
-// E: 1. If we are given "cat", and "dog", should return [cat dog]
-// E: 2. If we are given "software", and "engineer", should return [software engineer]
-// E: 3. If wer are given "fuck", and "you", should return [fuck you]
-// E: 4. If we are given "hakuna", and "matata", should return [hakuna matata]
-
-
-// use template literals to allow variables in the string
-
-function joinStrings(string1, string2){
-  // code here
- return `${string1} ${string2}`
-}
