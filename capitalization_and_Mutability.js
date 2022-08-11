20ee135a27e08732f82a65324596aee8c9d5c4a5
Sandy Cheng <cheek.codes@gmail.com>
2022-08-10 23:37:21 -0400

    four

--- capitalization_and_Mutability.js
+++ capitalization_and_Mutability.js
@@ -34,3 +34,8 @@ function capitalizeWord(word) {
 function capitalizeWord(word) {
     return word[0].toUpperCase() + word.slice(1);
 }
+
+
+
+
+const capitalizeWord = (word) => word.replace(word.charAt(0), word.charAt(0).toUpperCase());
