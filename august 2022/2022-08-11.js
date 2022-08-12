791266e43b5aa7f65d9eebbb71aec2a0fef24dd5
Sandy Cheng <cheek.codes@gmail.com>
2022-08-11 21:12:34 -0400

    one

---
+++ august 2022/2022-08-11.js
@@ -0,0 +1,26 @@
+// Abbreviate a two word name
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters witha  dot spearating them.
+
+// It should look like this:
+// [Sam Harris] --> [S.H]
+// [patrick feeney] --> [P.F]
+
+
+
+// P: Will I be given a special character? Will there be numbers such as floats and integers? Will there be an empty string? Will we only be given strings?
+
+
+
+// R: Return with capital first initial, [.], and capital last initial case insensitive
+
+
+
+// E: 1. If we are given "john doe", should return "J.D"
+// E: 2. If we are given "donald duck", should return "D.D"
+// E: 3. If we are given "princess bubblegum", should return "P.B"
+
+
+
