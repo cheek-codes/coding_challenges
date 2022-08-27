83bf86ad7d5096d0659fa77a53e5b3c39d0cc07c
Sandy Cheng <cheek.codes@gmail.com>
2022-08-26 23:01:58 -0400

    one

--- august 2022/2022-08-26.js
+++ august 2022/2022-08-26.js
@@ -1,1 +1,47 @@
- 
+// Grasshopper - Basic function fixer
+
+// Fix the function
+
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+/* 
+function addFive(num){
+    var total = num + 5
+    return num
+}
+*/
+
+
+
+// P: Will we be given a string? Will we be given an empty string? Will we be given a float? Will we be given negative integers? Will we be given positive numbers? Will we only be expected to just fix the function?
+
+
+
+// R: Return a new number with 5 added to it.
+
+
+
+// E: 1. If we are given 5, should return 10
+// E: 2. If we are given 0, should return 5
+// E: 3. If we are given -5, should return 0
+
+
+
+/* 
+function addFive(num){
+    var total = num + 5
+    return num
+}
+*/
+
+
+function addFive(num) {
+    var total = num + 5
+    // fix the above function by replacing num with total
+    return total
+  }
+
+
+
