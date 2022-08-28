3b366f33be456585d4e54f17fb8ff529bb1eceeb
Sandy Cheng <cheek.codes@gmail.com>
2022-08-27 23:15:49 -0400

    one

---
+++ august 2022/2022-08-27.js
@@ -0,0 +1,23 @@
+// Even or odd
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers of "Odd" for odd numbers.
+
+
+
+// P: Will we be given an empty string? Will we be given a string? Will we be given a float? Will we be given an integer? Will we be given a negative integer? Will we be given a positive integer?
+
+
+
+// R: Return "Even" if the given integer is an even number (if the given number is divisible by 2 and has no remainders)
+// R: Return "Odd" if the given integer is an odd number (if the given number is not divisible by 2 and has remainders)
+
+
+
+// E: 1. If we were given 2, should return "Even"
+// E: 2. If we were given 7, should return "Odd"
+// E: 3. If we were given -42, should return "Even"
+// E: 4. If we were given -7, should return "Odd"
+// E: 5. If we were given 0, should return "Even"
+
+
+
