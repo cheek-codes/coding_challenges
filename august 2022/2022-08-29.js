80f51f8fcaa326b2424e188acb55a3e9c9387fc3
Sandy Cheng <cheek.codes@gmail.com>
2022-08-29 12:46:12 -0400

    one

---
+++ august 2022/2022-08-29.js
@@ -0,0 +1,31 @@
+// Vowel remover
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// don't worry about uppercase vowels
+
+// y is not considered a vowel for this kata
+
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+
+
+
+// P: Will we be given numbers such as floats and integers? Will we be given empty strings? Will we be given uppercase letters? Will we be given lowercase letters? Will y be considered vowels?
+
+
+
+// R: Return a new string that does not have a lowercase vowel. Y is not considered a vowel in this case.
+
+
+
+// E: 1. If we are given "hello", should return "hll"
+// E: 2. If we are given "codewars", should return "cdwrs"
+// E: 3. If we are given "goodbye", should return "gdby"
+// E: 4. If we are given "HELLO", should return "HELLO"
+
+
+
