c06d2ad58d9dfa4dad13dc36bd879a60536ec729
Sandy Cheng <cheek.codes@gmail.com>
2022-08-28 23:36:53 -0400

    one

---
+++ august 2022/2022-08-28.js
@@ -0,0 +1,20 @@
+// Simple multiplication
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+
+// P: Will we be given an empty string? Will we be given a string? Will we be given floats? Will we be given a negative integer? Will we be given a positive integer?
+
+
+
+// R: Return a new number that was multiplied by 8 if that given number was even
+// R: Return a new number that was multiplied by 9 if that given number was odd.
+
+
+
+// E: 1. If we are given 2, should return 16 because 2 * 8 = 16
+// E: 2. If we are given 1, should return 9 because 1 * 9 = 9
+
+
+
