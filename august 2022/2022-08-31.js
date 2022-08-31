380091fdb7df99e0b1955e043f34d795e023a1ec
Sandy Cheng <cheek.codes@gmail.com>
2022-08-31 13:08:12 -0400

    one

--- august 2022/2022-08-31.js
+++ august 2022/2022-08-31.js
@@ -10,3 +10,17 @@
 
 // Note:
 // Names given are always valid strings.
+
+
+
+// P: Will we be given numbers such as floats and integers? Will we be given empty strings? Will we be given uppercase letters? Will we be given lowercase letters?
+
+
+
+// E: 1. If we were given "Randy", should return "Randy plays banjo"
+// E: 2. If we were given "randy", should return "randy plays banjo"
+// E: 3. If we were given "Sandy", should return "Sandy does not play banjo"
+// E: 4. If we were given "sandy", should return "sandy does not play banjo"
+
+
+
