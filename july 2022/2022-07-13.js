97b0a572680d1d21d504185c083d35ac3fccb5c9
Sandy Cheng <cheek.codes@gmail.com>
2022-09-06 21:53:35 -0400

    brute force one

--- july 2022/2022-07-13.js
+++ july 2022/2022-07-13.js
@@ -72,3 +72,19 @@
 // 8 * 10 = 80
 // 9 * 10 = 90
 // 10 * 10 = 100
+
+
+
+let multiTable = n => {
+    // create a template literal for every number from 1 - 10 for a multiplication table. #brootle
+    return `1 * ${n} = ${1*n}
+    2 * ${n} = ${2*n}
+    3 * ${n} = ${3*n}
+    4 * ${n} = ${4*n}
+    5 * ${n} = ${5*n}
+    6 * ${n} = ${6*n}
+    7 * ${n} = ${7*n}
+    8 * ${n} = ${8*n}
+    9 * ${n} = ${9*n}
+    10 * ${n} = ${10*n}`
+}
