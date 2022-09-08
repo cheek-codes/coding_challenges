081dd76f5b344516b1345de4541849ffb958cffa
Sandy Cheng <cheek.codes@gmail.com>
2022-09-07 21:56:09 -0400

    three

--- july 2022/2022-07-13.js
+++ july 2022/2022-07-13.js
@@ -103,3 +103,14 @@ const multiTable = (number) => {
     }
     return table
 }
+
+
+
+// try
+function multiTable(number) {
+    let table = '';
+    for(let i = 1; i <= 10; i++){
+      table += `${i} * ${number} = ${i*number}\n`
+    }
+    return table.slice(0, -1)
+}
