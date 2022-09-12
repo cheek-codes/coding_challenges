fe722521d2bca76f768a59979ace0543c3819272
Sandy Cheng <cheek.codes@gmail.com>
2022-09-11 22:09:08 -0400

    two

--- july 2022/2022-07-11.js
+++ july 2022/2022-07-11.js
@@ -35,3 +35,17 @@ function feast(beast, dish) {
         return false
     }
 }
+
+
+
+function feast(beast, dish) {
+    return beast.first() + beast.last() == dish.first() + dish.last();
+  }
+  
+  String.prototype.first = function() {
+    return this[0];
+  }
+  
+  String.prototype.last = function() {
+    return this[this.length-1];
+}
