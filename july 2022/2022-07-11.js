13d0184c935edc4a4816ed61eb2631b11fe6455c
Sandy Cheng <cheek.codes@gmail.com>
2022-09-13 22:10:51 -0400

    six

--- july 2022/2022-07-11.js
+++ july 2022/2022-07-11.js
@@ -76,3 +76,9 @@ function feast(beast, dish) {
 function feast(beast, dish) {
     return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
 }
+
+
+
+function feast(beast, dish) {
+    return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
+}
