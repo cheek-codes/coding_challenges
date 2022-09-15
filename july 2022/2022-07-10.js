2cff388c4f52dbe3091d84e2e87cfa54c9162543
Sandy Cheng <cheek.codes@gmail.com>
2022-09-14 22:18:56 -0400

    prompt

---
+++ july 2022/2022-07-10.js
@@ -0,0 +1,9 @@
+// Define a card suit
+
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// our deck is preloaded:
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
