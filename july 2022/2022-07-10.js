9c060da07a17167f75eceb90dcca3de1155af195
Sandy Cheng <cheek.codes@gmail.com>
2022-09-15 22:20:21 -0400

    parameter

--- july 2022/2022-07-10.js
+++ july 2022/2022-07-10.js
@@ -7,3 +7,7 @@
 // ('3♦') -> return 'diamonds'
 // ('3♥') -> return 'hearts'
 // ('3♠') -> return 'spades'
+
+
+
+// P: Will we be given strings? Will we be given an empty string? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers? Will we be given special characters?
