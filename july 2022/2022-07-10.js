60f3630cb76dae75fb21dcac2e0c149a8e26f8d7
Sandy Cheng <cheek.codes@gmail.com>
2022-09-17 22:26:49 -0400

    two

--- july 2022/2022-07-10.js
+++ july 2022/2022-07-10.js
@@ -43,3 +43,14 @@ function defineSuit(card) {
       return "spades"
     }
 }
+
+
+
+function defineSuit(card) {
+    switch (card.slice(-1)){
+      case '♣': return 'clubs'
+      case '♦': return 'diamonds'
+      case '♥': return 'hearts'
+      case '♠': return 'spades';   
+    }
+}
