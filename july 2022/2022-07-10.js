f72a891d4d060d3243a04fb7bdfc5e50acabf7ed
Sandy Cheng <cheek.codes@gmail.com>
2022-09-19 22:28:56 -0400

    six

--- july 2022/2022-07-10.js
+++ july 2022/2022-07-10.js
@@ -89,3 +89,14 @@ function defineSuit(card) {
     if(card.includes('♣')) return 'clubs'
     if(card.includes('♠')) return 'spades' 
 }
+
+
+
+function defineSuit(card) {
+    return {
+      '♣' : 'clubs',
+      '♦' : 'diamonds',
+      '♥' : 'hearts',
+      '♠' : 'spades'
+    }[card.substr(-1)]
+}
