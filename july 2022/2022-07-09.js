d7d58dbe2bd41bc0670acf424a8e7b43eb6dd180
Sandy Cheng <cheek.codes@gmail.com>
2022-09-20 22:33:08 -0400

    parameter

--- july 2022/2022-07-09.js
+++ july 2022/2022-07-09.js
@@ -1,3 +1,7 @@
 // Convert a boolean to a string
 
 // Implement a function which convert the given boolean value into its string representation.
+
+
+
+// P: Will we be given a string? Will we be given an empty string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers? Will we be given booleans?
