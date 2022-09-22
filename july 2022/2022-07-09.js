4d599071e9504a174bba83993d1dad6273efa6d6
Sandy Cheng <cheek.codes@gmail.com>
2022-09-21 22:38:09 -0400

    one

--- july 2022/2022-07-09.js
+++ july 2022/2022-07-09.js
@@ -16,3 +16,18 @@
 // E: 2. If we are given false, should return as "false"
 // E: 3. If we are given "2 === 3", should return "false"
 // E: 4. If we are givne "2 === 2", should return "true"
+
+
+
+function booleanToString(b){
+    // create an if else statement
+    // if b is true, return it as a string of "true"
+    if(b === true){
+      return "true";
+    }
+    
+    // if b is false, return it as string "false"
+    else{
+      return "false";
+    }
+}
