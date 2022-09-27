d0c1abd440804bf23bafba4475a6ab5f3ef45e37
Sandy Cheng <cheek.codes@gmail.com>
2022-09-27 16:39:40 -0400

    returns

--- july 2022/2022-07-14.js
+++ july 2022/2022-07-14.js
@@ -14,3 +14,16 @@
 
 
 
+// R: Return a "Zero" if the given parameter is 0
+// R: Return a "One" if the given parameter is 1
+// R: Return a "Two" if the given parameter is 2
+// R: Return a "Three" if the given parameter is 3
+// R: Return a "Four" if the given parameter is 4
+// R: Return a "Five" if the given parameter is 5
+// R: Return a "Six" if the given parameter is 6
+// R: Return a "Seven" if the given parameter is 7
+// R: Return a "Eight" if the given parameter is 8
+// R: Return a "Nine" if the given parameter is 9
+
+
+
