0e950b24cc1856b6d0a82691997f49da583b5b8f
Sandy Cheng <cheek.codes@gmail.com>
2022-09-27 16:35:14 -0400

    parameter

--- july 2022/2022-07-14.js
+++ july 2022/2022-07-14.js
@@ -7,3 +7,10 @@
 // Output: "One"
 
 // If your language supports it, try using a switch statement
+
+
+
+// P: Will we be given a string? Will we be given an empty string? Will we be given a special character? Will we be given floats? Will we be given integers?
+
+
+
