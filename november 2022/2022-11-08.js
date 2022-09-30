e4793521a0fa219b3db0dc77ede60b1bc8091f24
Sandy Cheng <cheek.codes@gmail.com>
2022-09-30 15:16:41 -0400

    parameters

--- november 2022/2022-11-08.js
+++ november 2022/2022-11-08.js
@@ -1,3 +1,10 @@
 // Super Duper Easy
 
 // Make a function that returns the value multiplies by 50 and increased by 6. If the value entered is a string it should return "Error"
+
+
+
+// P: Will we be given a string? Will we be given an empty string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given postitive number?
+
+
+
