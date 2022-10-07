d86e061c34de665093894fe16a4cf735f09ffdb4
Sandy Cheng <cheek.codes@gmail.com>
2022-10-06 22:11:49 -0400

    parameter

--- july 2022/2022-07-16.js
+++ july 2022/2022-07-16.js
@@ -8,3 +8,7 @@
 // 123  --> "123"
 // 999  --> "999"
 // -100 --> "-100"
+
+
+
+// P: Will we be given strings? Will we be given an empty string? Will we be given special characters? Will we be given a floats? Will we be given an integer? Will we be given negative numbers? Will we be given positive numbers
