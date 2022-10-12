89ed0a7e388eb50f1f4e9906ddd0137b89929db5
Sandy Cheng <cheek.codes@gmail.com>
2022-10-11 21:26:18 -0400

    parameter

--- july 2022/2022-07-19.js
+++ july 2022/2022-07-19.js
@@ -1,3 +1,7 @@
 // Remove String Spaces
 
 // Simple, remove the spaces fromt he string, then return the resultant string.
+
+
+
+// P: Will we be given a float? Will we be given an integer? Will we be given an empty string? Will we be given captial letters? Will we be given lower case letters? 
