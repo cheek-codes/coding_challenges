631dbd02f7a759e7682871ec55257eb15ef42cc5
Sandy Cheng <cheek.codes@gmail.com>
2022-10-13 21:38:04 -0400

    examples

--- july 2022/2022-07-19.js
+++ july 2022/2022-07-19.js
@@ -5,3 +5,16 @@
 
 
 // P: Will we be given a float? Will we be given an integer? Will we be given an empty string? Will we be given captial letters? Will we be given lower case letters? 
+
+
+
+// R: Return a new string that has removed the given string parameter's spaces
+
+
+
+// E: 1. If we are given a string parameter of "Interview with a vampire", should return "Interviewwithavampire"
+// E: 2. If we are given a string parameter of "nine perfect strangers", should return "nineperfectstrangers"
+// E: 3. If we are given a string parameter of "Harry Potter and the Half Blood Prince", should return "HarryPotterandtheHalfBloodPrince"
+
+
+
