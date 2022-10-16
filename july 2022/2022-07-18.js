91a28b696e4f1e1df7d21cf283aac6776354a12c
Sandy Cheng <cheek.codes@gmail.com>
2022-10-15 21:49:15 -0400

    parameters

--- july 2022/2022-07-18.js
+++ july 2022/2022-07-18.js
@@ -5,3 +5,7 @@
 // Examples (input->output)
 // 6, "I"->"IIIIII"
 // 5, "Hello"->"HelloHelloHelloHelloHello"
+
+
+
+// P: Will we be given strings? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers? Will we be given special characters? Will we be given empty strings? Will we be given arrays?
