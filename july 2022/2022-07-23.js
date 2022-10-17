08a3d32225993e627f4bcb2927fee324aaee53f2
Sandy Cheng <cheek.codes@gmail.com>
2022-10-17 13:17:45 -0400

    parameter

--- july 2022/2022-07-23.js
+++ july 2022/2022-07-23.js
@@ -1,3 +1,7 @@
 // Remove First and Last Character
 
 // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+
+
+// P: Will we be given numbers such as floats and integers? Will we be given empty strings? Will we be given uppercase letters? Will we be given lowercase letters?
