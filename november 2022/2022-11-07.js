4e096af8cc780c0c515bca4745d442b28318b790
Sandy Cheng <cheek.codes@gmail.com>
2022-10-19 21:02:27 -0400

    lesser lines

--- november 2022/2022-11-07.js
+++ november 2022/2022-11-07.js
@@ -69,3 +69,8 @@ function finalGrade (exam, projects) {
     if(exam > 50 & projects >= 2) return 75;
     return 0;
 }
+
+
+
+// lesser lines
+const finalGrade = (grade, projects) => grade > 90 || projects > 10 ? 100 : grade > 75 && projects > 4 ? 90 : grade > 50 && projects > 1 ? 75 : 0;
