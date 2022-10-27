3a13d55a6f44b942c375c6908edc2c401e0bb356
Sandy Cheng <cheek.codes@gmail.com>
2022-10-26 23:16:07 -0400

    one

--- Multiply_the_number.js
+++ Multiply_the_number.js
@@ -24,3 +24,22 @@
 // E: 3. If we are given 200, should return 25000 because 200 * 5^3
 // E: 4. If we are given 0, should return 0 because 0 * 5^1
 // E: 5. If we are given -3, should return -15 because -3 * 5^1
+
+
+
+
+function multiply(number){
+    // create a variable that turns an integer into a string and see how many characters are in it
+    let num = number.toString().length
+
+    // create an if else statement. if the given number is greater than or equal to 0 aka does not have a negative sign, return number * (5**num)
+    if (number >= 0){
+      return number * (5**num)
+    }
+
+    // if the given number is negative aka has an extra character because of the negative sign, take away that extra character
+    else if (number < 0){
+      num = num -1
+      return number * (5**num)
+    }
+}  
