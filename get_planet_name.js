30183a44dd164ba859a2ffb85696664e94ee95db
Sandy Cheng <cheek.codes@gmail.com>
2022-10-28 16:00:12 -0400

    four

--- get_planet_name.js
+++ get_planet_name.js
@@ -112,3 +112,28 @@ function getPlanetName(id){
     }
     return name
 }
+
+
+
+function getPlanetName(id){  
+    switch(id){
+      case 1:
+        return "Mercury";
+      case 2:
+        return "Venus";
+      case 3:
+        return "Earth";
+      case 4:
+        return "Mars";
+      case 5:
+        return "Jupiter";
+      case 6:
+        return "Saturn";
+      case 7:
+        return "Uranus";
+      case 8:
+        return "Neptune";
+      default:
+        return "invalid planet id in function getPlanetName()";
+    }
+}
