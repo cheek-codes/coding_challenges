f08d7207424a5a5eef612aae6a5b7df6bd12e5b4
Sandy Cheng <cheek.codes@gmail.com>
2022-10-31 21:35:34 -0400

    template

---
+++ august 2022/2022-08-13.js
@@ -0,0 +1,29 @@
+// Name Shuffler
+
+// Write a function that returns a string in which firsname is swapped with lastname.
+
+// example(input --> output)
+// "john McClane" --> "McClane john"
+
+
+
+// P: Will I be given special character? Will I be given numbers such as floats and integers? Will there be empty strings? Will I only be given strings?
+
+
+
+// R: Return as [lastname firstname]
+
+
+
+// E: 1. If we are given "john doe", should return "doe john"
+// E: 2. If we are given "donald duck", should return "duck donald"
+// E: 3. If we are given "princess bubblegum", should return "bubblegum princess"
+
+
+
+function nameShuffler(str){
+    // split the string into separate strings from the space in between them
+    // reverse the order of the string
+    // then join the strings back together
+    return str.split(" ").reverse().join(" ")
+}
