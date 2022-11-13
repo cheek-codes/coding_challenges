fd038f6e35136d7df2ff2ede351486eb27f47f5d
Sandy Cheng <cheek.codes@gmail.com>
2022-11-13 13:18:03 -0500

    return

--- square_every-digit.js
+++ square_every-digit.js
@@ -9,3 +9,7 @@
 
 
 // P: Will we be gvien empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return a single integer where the square of every digit of a given number is squared and concatenated.
