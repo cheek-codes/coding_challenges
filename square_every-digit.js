cfbb61a27a0f144440d40860658d34b3a8ee2d76
Sandy Cheng <cheek.codes@gmail.com>
2022-11-13 13:16:58 -0500

    parameter

--- square_every-digit.js
+++ square_every-digit.js
@@ -5,3 +5,7 @@
 // For example, if we run 9119 throught the function, 81181 will come out because 9^2 is 81 and 1^2 is 1.
 
 // Note: The function accepts an integer and retruns an integer
+
+
+
+// P: Will we be gvien empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
