0f015375f828bf815b7e110dac8cb197a6004a95
Sandy Cheng <cheek.codes@gmail.com>
2022-11-14 13:15:23 -0500

    prompt

---
+++ square_every-digit.js
@@ -0,0 +1,7 @@
+// Square Every Digit
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 throught the function, 81181 will come out because 9^2 is 81 and 1^2 is 1.
+
+// Note: The function accepts an integer and retruns an integer
