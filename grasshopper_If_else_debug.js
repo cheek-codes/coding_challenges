0855103d0ce42bbfa2ddfe02f358cde8dac27d99
Sandy Cheng <cheek.codes@gmail.com>
2022-11-18 12:26:24 -0500

    parameter

--- grasshopper_If_else_debug.js
+++ grasshopper_If_else_debug.js
@@ -5,3 +5,7 @@
 // checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
 
 // The function receives one parameter health which will always be a whole number between -10 and 10.
+
+
+
+// P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
