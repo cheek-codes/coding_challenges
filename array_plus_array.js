f221722b6929b1d2ee0f3160f21a48819bc24c0b
Sandy Cheng <cheek.codes@gmail.com>
2022-11-19 14:35:38 -0500

    prompt

---
+++ array_plus_array.js
@@ -0,0 +1,5 @@
+// Array plus array
+
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// PS Each array includes onl integer numbers. Output is a number too.
