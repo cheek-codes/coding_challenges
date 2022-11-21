177f9c0ee4c771aeaeee3dc5767dcd9be8328afd
Sandy Cheng <cheek.codes@gmail.com>
2022-11-21 12:37:16 -0500

    two

--- a_bugs_trilogy_ep1.js
+++ a_bugs_trilogy_ep1.js
@@ -47,3 +47,7 @@ yourFutureCareer()
 
 
 
+const yourFutureCareer = () => {
+    let career = Math.random()
+    return `${career <= 0.32 ? "FrontEnd" : (career <= 0.65 ? "BackEnd" : "Full-Stack")} Developer`
+}
