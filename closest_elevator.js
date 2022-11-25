da14be42f26c82da7a243fadc428a627f5abc198
Sandy Cheng <cheek.codes@gmail.com>
2022-11-24 22:43:17 -0500

    examples

--- closest_elevator.js
+++ closest_elevator.js
@@ -27,3 +27,11 @@
 
 // R: Return the name of the elevator closest to the called floor.
 // R: Return the elevator to the right if both elevators are equally distant form the called floor.
+
+
+
+// E: 1. If we were given elevator(0, 1, 0), should return "left"
+// E: 2. If we were given elevator(0, 1, 1), should return "right"
+// E: 3. If we were given elevator(0, 1, 2), should return "right"
+// E: 4. If we were given elevator(0, 0, 0), should return "right"
+// E: 5. If we were given elevator(0, 2, 1), should return "right"
