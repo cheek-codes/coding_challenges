60db150a195ff4122d5e0002a1d057503036dba3
Sandy Cheng <cheek.codes@gmail.com>
2022-11-25 11:43:51 -0500

    first commit

---
+++ makeUpperCase.js
@@ -0,0 +1,17 @@
+// MakeUpperCase
+
+// Write a function which converts the input string touppercase.
+
+
+
+// P: Will we be given floats? Will we be given integers? Will we be given special characters? Will we be given strings? Will we be given an empty string?
+
+
+
+// R: Return a new string that has converted the given string to uppercase
+
+
+
+//: E: 1. If we were given "hello", should return "HELLO"
+//: E: 1. If we were given "HeLlo", should return "HELLO"
+//: E: 1. If we were given "HELLO", should return "HELLO"
