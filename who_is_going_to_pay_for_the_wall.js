9eac8a80ea8be86669d7ba518d3538c13269435a
Sandy Cheng <cheek.codes@gmail.com>
2022-11-26 20:21:44 -0500

    new

---
+++ who_is_going_to_pay_for_the_wall.js
@@ -0,0 +1,21 @@
+// Who is going to pay for the wall?
+
+// Don Drumphet lives in a nice neighborhood, but one of his neighbors has started to let his house go. Don Drumphet wants to build a wall between his house and his neighbor’s, and is trying to get the neighborhood association to pay for it. He begins to solicit his neighbors to petition to get the association to build the wall. Unfortunately for Don Drumphet, he cannot read very well, has a very limited attention span, and can only remember two letters from each of his neighbors’ names. As he collects signatures, he insists that his neighbors keep truncating their names until two letters remain, and he can finally read them.
+
+// Your code will show Full name of the neighbor and the truncated version of the name as an array. If the number of the characters in name is less than or equal to two, it will return an array containing only the name as is"
+
+
+
+// P: Will we be given an empty string? Will we be given strings? Will we be given floats? Will we be given integers? Will we be given special characters?
+
+
+
+// R: Retrun the name if the given name has 2 or less characters
+// R: Retrun a new string with the full name as well as the first two characters if the given name has more than 2 characters
+
+
+
+// E: 1. If we are given "Mexico", should return ["Mexico", "Me"]
+// E: 2. If we are given "Melania", should return ["Melania", "Me"]
+// E: 3. If we are given "", should return [""]
+// E: 4. If we are given "I", should return ["I"]
