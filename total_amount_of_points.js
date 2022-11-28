7b946eaf1e984e523d084423d292a2be9cc1679f
Sandy Cheng <cheek.codes@gmail.com>
2022-11-28 15:17:45 -0500

    ans

--- total_amount_of_points.js
+++ total_amount_of_points.js
@@ -89,3 +89,34 @@ function points(games) {
     }
     return totalPoints
 }
+
+
+
+
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+
+// function points(games) {
+//   return games.reduce((output,current)=>{
+//     let x = parseInt(current[0]);
+//     let y = parseInt(current[2]);
+//     let value= x>y ? 3 : x===y ? 1 : 0;
+//     return output+value;
+//   },0)
+// }
+
+
+
+
+function points(games) {
+    let total = 0;
+    games.map(game => {
+      if (game[0] === game[2]) {
+        total += 1;
+      } else if (game[0] > game[2]) {
+        total += 3;
+      }
+    });
+    return total;
+}
