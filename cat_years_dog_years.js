aed87c6f8bf7bd6072473fb20d6cc6e367e58f7e
Sandy Cheng <cheek.codes@gmail.com>
2022-11-30 19:59:55 -0500

    answers

--- cat_years_dog_years.js
+++ cat_years_dog_years.js
@@ -52,3 +52,119 @@ var humanYearsCatYearsDogYears = function(humanYears) {
 
 
 
+var humanYearsCatYearsDogYears = function(y) {
+    if (y == 1) return [1, 15, 15]
+    if (y == 2) return [2, 24, 24]
+    return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
+}
+
+
+
+
+const humanYearsCatYearsDogYears = (humanYears) => {
+    let catYears = 0;
+    let dogYears = 0;
+    
+    for (let i = 1; i <= humanYears; i++) {
+      if (i === 1) {
+        catYears += 15;
+        dogYears += 15;
+      }
+      else if (i === 2) {
+        catYears += 9;
+        dogYears += 9;
+      }
+      else {
+        catYears += 4;
+        dogYears += 5;
+      }
+    }
+    return [humanYears,catYears,dogYears];
+}
+
+
+
+
+const humanYearsCatYearsDogYears = humanYears => [
+    humanYears,
+    ( humanYears - 1 ? 16 : 11 ) + 4 * humanYears,
+    ( humanYears - 1 ? 14 : 10 ) + 5 * humanYears,
+]
+
+
+
+
+function humanYearsCatYearsDogYears(humanYears) {
+    switch (humanYears) {
+      case 1:
+        return [1, 15, 15]
+      case 2:
+        return [2, 24, 24];
+      default:
+        return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24 ];
+    }
+}
+
+
+
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    var catYears = 0;
+    var dogYears = 0;
+    
+    if (humanYears === 1) {
+      catYears = 15;
+      dogYears = 15;
+    } else if (humanYears === 2) {
+      catYears = 24;
+      dogYears = 24;
+    } else if (humanYears > 2) {
+      catYears = 24 + (humanYears - 2) * 4;
+      dogYears = 24 + (humanYears - 2) * 5;
+    }
+  
+    return [humanYears, catYears, dogYears];
+}
+
+
+
+
+var humanYearsCatYearsDogYears = x => x == 1 ? [1, 15, 15] : x == 2 ? [2, 24, 24] : [x , 24 + ( 4 * (x-2)), 24 + (5 * (x-2))];
+
+
+
+
+const humanYearsCatYearsDogYears = h => [h, (h - 1 ? 16 : 11) + 4 * h, (h - 1 ? 14 : 10) + 5 * h];
+
+
+
+
+const humanYearsCatYearsDogYears = humanYears =>
+  [humanYears, (--humanYears ? 20 : 15) + 4 * humanYears, (humanYears ? 19 : 15) + 5 * humanYears];
+
+
+
+
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    if(humanYears >= 2) {
+      return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
+    }
+    return [humanYears, 15, 15];
+}
+
+
+
+
+const humanYearsCatYearsDogYears = $ => 
+      $ == 1 ? [1, 15, 15] : 
+      $ == 2 ? [2, 24, 24] : 
+      [$ , 24 + ( 4 * ($ - 2)), 24 + (5 * ($ - 2))];
+
+
+
+
+const humanYearsCatYearsDogYears = hYears => {
+    if (hYears === 1) return [1, 15, 15]
+        return [hYears, (hYears - 2) * 4 + 24, (hYears - 2) * 5 + 24]
+}
