4da572ae494f8bef91c94b8c18e6c8a61553dc77
Sandy Cheng <cheek.codes@gmail.com>
2022-12-04 21:28:45 -0500

    answers

--- 8 kyu/pillars.js
+++ 8 kyu/pillars.js
@@ -28,3 +28,59 @@
 
 
 
+function pillars(numPill, dist, width) {
+    // your code here
+    if (numPill >1){
+      return (numPill - 2) * width + (numPill-1) * dist * 100
+    }
+    else{
+      return 0
+    }
+}
+
+
+
+
+const pillars = (numPill, dist, width) => (numPill > 1) ? (numPill - 2) * width + (numPill-1) * dist * 100 : 0
+
+
+
+
+function pillars(num_pill, dist, width) {
+    if (num_pill < 2) return 0;
+    const widthAllPillars = (num_pill - 2) * width;
+    const distAll = (num_pill - 1) * (dist * 100);
+    
+    return distAll + widthAllPillars;
+}
+
+
+
+
+function pillars(num_pill, dist, width) {
+    return num_pill <= 1 ? 0 : (num_pill - 1) * dist * 100 + (num_pill - 2) * width;
+}
+
+
+
+
+const pillars = (n, d, w) => Math.max(0, n - 1) * (d * 100) + Math.max(0, n - 2) * w; 
+
+
+
+
+function pillars(num_pill, dist, width) {
+    if (num_pill === 1) {
+        return 0;
+    }else{
+        let distance = dist * ((num_pill -1) * 100 );
+        let pillars = num_pill - 2;
+        return distance + (width * pillars);
+    }  
+}
+
+
+
+
+const pillars = (num_pill, dist, width) =>
+  Math.max((dist * 100 + width) * --num_pill - width, 0);
