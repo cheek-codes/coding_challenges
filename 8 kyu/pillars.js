6e11365024fb391742c4e3cbefd2112bbc62c304
Sandy Cheng <cheek.codes@gmail.com>
2022-12-04 21:25:14 -0500

    pillars

---
+++ 8 kyu/pillars.js
@@ -0,0 +1,30 @@
+// Pillars
+
+// There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+// 1. number of pillare(>=1)
+// 2. distance between pillars (10 - 30 meters)
+// 3. with of the pillar (10 - 5- centimeters)
+
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar)
+
+
+
+
+// P: Will we be given an empty string? Will we be given strings? Will we be given special characters? Will we be given arrays? Will we be given an empty array? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+
+// R: Return the distance between the first and the last pillar in centimeters without the width of the first and last pillar
+
+
+
+
+// E: 1. If we were given pillars(numPills, dist, width) = pillars(1, 10, 10), should return 0
+// E: 2. If we were given pillars(numPills, dist, width) = pillars(2, 20, 25), should return 2000
+// E: 2. If we were given pillars(numPills, dist, width) = pillars(11, 15, 30), should return 15270
+
+
+
+
