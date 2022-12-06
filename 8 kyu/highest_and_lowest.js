19ce3fbc034fd6b36979965355221ed338b61b43
Sandy Cheng <cheek.codes@gmail.com>
2022-12-06 13:57:46 -0500

    answers

--- 8 kyu/highest_and_lowest.js
+++ 8 kyu/highest_and_lowest.js
@@ -30,3 +30,87 @@
 
 
 
+function highAndLow(numbers){
+    // split the string
+    let split = numbers.split(" ")
+    // find max and min 
+    let max = Math.max(...split)
+    let min = Math.min(...split)
+    // return
+    return `${max} ${min}`
+}
+
+
+
+
+
+function highAndLow(numbers){
+    numbers = numbers.split(' ').map(Number);
+    return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
+
+
+
+
+
+const highAndLow = numbers => `${Math.max(...numbers.split(' '))} ${Math.min(...numbers.split(' '))}`;
+
+
+
+
+
+function highAndLow(numbers){
+    numbers = numbers.split(' ');
+    return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+
+
+
+
+function highAndLow(numbers){
+    let arr = numbers.split(' ').map(Number);  
+    return Math.max(...arr) + ' ' + Math.min(...arr);
+}
+
+
+
+
+function highAndLow(numbers){
+    var arr = numbers.split(' ').sort(function(a, b) { return a - b });
+    return arr[arr.length -1] + ' ' + arr[0];
+}
+
+
+
+
+function highAndLow(numbers) {
+    var arr = numbers.split(" ");
+    return Math.max.apply(null, arr) + ' ' + Math.min.apply(null, arr);
+}
+
+
+
+
+function highAndLow(numbers){
+    numbers = numbers.split(' ').sort((a, b)=> Number(a)-Number(b))
+    return [numbers[numbers.length-1], numbers[0]].join(' ')
+}
+
+
+
+
+function highAndLow(numbers){
+    numbers = numbers.split(' ');
+    
+    return Math.max(...numbers) + ' ' + Math.min(...numbers);
+}
+
+
+
+
+function highAndLow(numbers) {
+    numbers = numbers.split(' ').map(Number);
+    const max = Math.max(...numbers);
+    const min = Math.min(...numbers);
+    return `${max} ${min}`;
+}
