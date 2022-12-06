a38db8b9fac7852ec499d771a10b991f009f6849
Sandy Cheng <cheek.codes@gmail.com>
2022-12-06 13:53:50 -0500

    highest and lowest

---
+++ 8 kyu/highest_and_lowest.js
@@ -0,0 +1,32 @@
+// Highest and lowest
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Example:
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+// Notes:
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string
+// Output string must be separated by a single space, and highest number is first
+
+
+
+
+// P: Will we be given empty strings/ Will we be given strings? Will we be given special characters? Will we be given an array? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+// R: Return a string of number separated by a space with the highest number first and the lowest number second
+
+
+
+// E: 1. If we were given highAndLow("1 2 3 4 5"), should return "5 1"
+// E: 2. If we were given highAndLow("1 2 -3 4 5"), should return "5 -3"
+// E: 3. If we were given highAndLow("1 9 3 4 -5"), should return "9 -5"
+
+
+
+
