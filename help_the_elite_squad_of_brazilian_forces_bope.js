6592f8e49ec8f073ebf22c1a51b4e135c1ef4523
Sandy Cheng <cheek.codes@gmail.com>
2022-12-07 13:56:29 -0500

    help the elite squad of brazilian forces bope

---
+++ help_the_elite_squad_of_brazilian_forces_bope.js
@@ -0,0 +1,40 @@
+// help the elite squad of brazilian forces BOPE
+
+// the BOPE is the squad of special forces of police that usually handles the operations in the Favelas in Rio de Janeiro.
+
+// In the kata you have to write a function that determine the number of magazines that every soldier has to have in his bag.
+
+// You will recieve the weapon and the number of streets that they have to cross. Considering that every street the officer shoots 3 times. Below is the relation of weapons:
+// PT92 - 17 bullets
+// M4A1 - 30 bullets
+// M16A2 - 30 bullets
+// PSG1 - 5 bullets
+
+// Example:
+// ["PT92", 6] ==> (6 streets, 3 bullets each)
+// ["M4A1", 6] => 1
+
+// The return will always be an integer so as the params. 
+
+
+
+
+// P: Will we be given strings? Will we be given empty strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive number?
+
+
+
+
+// R: Return an integer where it will show how many magazines that every soldier should carry
+
+
+
+
+// E: 1. If we were given magNumber["PT92", 6], should return 2
+// E: 2. If we were given magNumber["M4A1", 8], should return 1
+// E: 3. If we were given magNumber["M16A2", 19], should return 2
+// E: 4. If we were given magNumber["PSG1", 31], should return 19
+// E: 5. If we were given magNumber["PT92", 19], should return 4
+
+
+
+
