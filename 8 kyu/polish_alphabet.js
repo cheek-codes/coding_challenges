db5234c5e116a51797908f8b836371e76e3d012f
Sandy Cheng <cheek.codes@gmail.com>
2022-12-11 14:48:00 -0500

    polish alphabet

---
+++ 8 kyu/polish_alphabet.js
@@ -0,0 +1,37 @@
+// polish alphabet
+
+// There are 32 letters in the Polish alphabet: 9 vowels, and 23 consonants.
+
+// Your task is to change the letter with diacritics and print out the string without the use of the Polish letters:
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+
+// for example:
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+
+
+
+// P: Will we be given and empty string? Will we be given strings? Will we be given special characters? Will we be given an array? Will we be given floats? Will we be given integers?
+
+
+
+
+// R: Return a new string that replaces Polish letters with English letters
+
+
+
+
+// E: 2. If we were given "Jędrzej Błądziński", should return "Jedrzej Bladzinski"
+// E: 2. If we were given "Lech Wałęsa", should return "Lech Walesa"
+
+
+
+
