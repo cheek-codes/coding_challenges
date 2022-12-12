1abd822cbfcb57bc2d5cd6e426e89eb9f3af6cab
Sandy Cheng <cheek.codes@gmail.com>
2022-12-11 21:35:29 -0500

    answers

--- 8 kyu/polish_alphabet.js
+++ 8 kyu/polish_alphabet.js
@@ -35,3 +35,57 @@
 
 
 
+
+function correctPolishLetters (string) {
+    return string.replace(/ą/g, "a").replace(/ć/g, "c").replace(/ę/g, "e").replace(/ł/g, "l").replace(/ń/g, "n").replace(/ó/g, "o").replace(/ś/g, "s").replace(/ź/g, "z").replace(/ż/g, "z")
+}
+
+
+
+
+
+var polishLetters = {
+    "ą": "a",
+    "ć": "c",
+    "ę": "e",
+    "ł": "l",
+    "ń": "n",
+    "ó": "o",
+    "ś": "s",
+    "ź": "z",
+    "ż": "z",
+};
+
+function correctPolishLetters (string) {
+  return string.split('').map((c) => polishLetters[c] || c).join("");}
+
+
+
+
+
+function correctPolishLetters (string) {
+    var dict = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
+    return string.replace(/[ąćęłńóśźż]/g, match => dict[match]);
+}
+
+
+
+
+
+function correctPolishLetters(s) {
+    return s.replace(/[ąćęłńóśźż]/g, c => "acelnoszz"["ąćęłńóśźż".indexOf(c)])
+}
+
+
+
+
+
+const c1="ąćęłńóśźż",c2="acelnoszz",correctPolishLetters =s=>s.replace(/./g,x=>(y=c1.indexOf(x))+1?c2[y]:x)
+
+
+
+
+
+
+const correctPolishLetters = s =>
+  s.replace(/[ąćęłńóśźż]/g, val => `acelnoszz`[`ąćęłńóśźż`.indexOf(val)]);
