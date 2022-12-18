d5a486e96cd26a4834d0d1922da0fd9fdf6ad62d
Sandy Cheng <cheek.codes@gmail.com>
2022-12-17 21:29:39 -0500

    string templates bug fix 5

---
+++ 8 kyu/string_templates_bug_fixing_5.js
@@ -0,0 +1,37 @@
+// string templates - bug fixing #5
+
+// oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature. Help Timmy with his string tmeplate so it works as he expects!
+
+/*
+function buildString(...template){
+    return `I like #{template.join(',')}!`;
+}
+*/
+
+
+
+
+// Will we be given string? Will we be given empty string? Will we be given special characters? Will we be given an array? Will we be given floats? Will we be given integers?
+
+
+
+
+// Return a new string where it takes in a parameter using string template.
+
+
+
+
+// 1. If we were given 'Cheese','Milk','Chocolate'), should return 'I like Cheese, Milk, Chocolate!'
+
+
+
+
+function buildString(...template){
+    return `I like ${template.join(', ')}!`;
+}
+
+
+
+
+
+const buildString = (...template) => `I like ${template.join(', ')}!`;
