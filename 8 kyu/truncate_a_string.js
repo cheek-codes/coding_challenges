fa6d5b709c06db6a9b0d6be5b3a66cbce3e2fafb
Sandy Cheng <cheek.codes@gmail.com>
2022-12-22 22:25:20 -0500

    truncate a string

---
+++ 8 kyu/truncate_a_string.js
@@ -0,0 +1,31 @@
+// truncate a string
+
+// Truncate the given string(first argument) if it is longer than the given maximum length (second argument). Return the truncated string with a "..." ending.
+
+// Nothe that inserting the three dots to the end will add to the string length.
+
+// However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string. 
+
+// Examples
+// ('codewars', 9)  ==>  'codewars'
+// ('codewars', 7)  ==>  'code...'
+// ('codewars', 2)  ==>  'co...'
+
+
+
+
+// Will we be given strings? Will we be given empty strings? Will we be given special characters? Will we be given floats? Will we be given integers?
+
+
+
+
+// Return a new string where it is truncated with a "..." ending if the maximum length is less than or equal to 3 without adding to the string length.
+// Return a new string where it is truncated with a "..." ending if the maximum length is more than 3 that will add ... to the total length of the string
+// Return the string without "..." ending if the maximum length is the same number of characters as the string.
+
+
+
+
+// If we were given ('codewars', 9), should return 'codewars'
+// If we were given ('codewars', 7), should return 'code...'
+// If we were given ('codewars', 2), should return 'co...'
