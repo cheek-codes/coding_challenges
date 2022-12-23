fd0902b39a0b713cb0fae4653dc5c99b1e1b6cae
Sandy Cheng <cheek.codes@gmail.com>
2022-12-22 23:04:31 -0500

    truncate a string ans

--- 8 kyu/truncate_a_string.js
+++ 8 kyu/truncate_a_string.js
@@ -29,3 +29,73 @@
 // If we were given ('codewars', 9), should return 'codewars'
 // If we were given ('codewars', 7), should return 'code...'
 // If we were given ('codewars', 2), should return 'co...'
+
+
+
+
+
+
+function truncateString(str, num) {
+    // if num <=3 return str +...
+    if (num <= 3){
+      return `${str.slice(0, num)}...`
+    }
+    // if num >= str.length return str
+    else if (num >= str.length){
+      return `${str}`
+    }
+    // if num > 3 return str.length -3 + ...
+    else if (num > 3){
+      return `${str.slice(0, num-3)}...`
+    }
+}
+
+
+
+
+
+
+function truncateString(str, num) {
+    if (num <= 3) {
+        return str.slice(0, num).concat("...");
+    } else if (str.length > num) {
+      return str.slice(0, ( num - 3)).concat("...");
+    } else {
+      return str; 
+    }
+}
+
+
+
+
+
+
+function truncateString(str, num) {
+    return str.length > num ? str.slice(0,num>3 ? num-3 : num)+"..." : str
+}
+
+
+
+
+
+const truncateString = (str, num) =>
+  str.length > num ? str.slice(0, num - (num > 3) * 3) + `...` : str;
+
+
+
+
+
+
+function truncateString(str, num) {
+ if (str.length <= num)
+    return str;
+
+ return str.substr(0, (num <= 3 ? num : num-3)) + '...';
+}
+
+
+
+
+
+
+const truncateString=(str, num)=>num>=str.length ? str : num>3 ? str.slice(0,num-3)+'...' : str.slice(0,num)+'...';
