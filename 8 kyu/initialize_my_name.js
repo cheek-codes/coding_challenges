e83b14b01dcff514a6239c78ded1b47237ea2d0e
Sandy Cheng <cheek.codes@gmail.com>
2022-12-23 22:16:20 -0500

    initialize my name

---
+++ 8 kyu/initialize_my_name.js
@@ -0,0 +1,32 @@
+// Initialize my name
+
+
+// Some people just have a first name; some people have first and last names, and some people have first, middle, and last names.
+
+// Your task is to initialize the middel names (if there are any)
+
+// Examples
+// 'Jack Ryan'                   => 'Jack Ryan'
+// 'Lois Mary Lane'              => 'Lois M. Lane'
+// 'Dimitri'                     => 'Dimitri'
+// 'Alice Betty Catherine Davis' => 'Alice B. C. Davis'
+
+
+
+
+
+// Will we be given strings? Will we be given empty strings? Will we be given special characters? Will we be given floats? Will we be given integers?
+
+
+
+
+// Return a new string where the middle name(s) are initialized
+
+
+
+
+
+// If we were given 'Jack Ryan', should return 'Jack Ryan'
+// If we were given 'Lois Mary Lane', should return 'Lois M. Lane'
+// If we were given 'Dimitri', should return 'Dimitri'
+// If we were given 'Alice Betty Catherine Davis', should return 'Alice B. C. Davis'
