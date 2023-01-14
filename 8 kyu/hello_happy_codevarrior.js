8cd0b589286b0c8084a65fa381b91b11f9ac0c19
cheek-codes <cheek.codes@gmail.com>
2023-01-13 22:37:29 -0500

    hello happy codevarrior

--- 8 kyu/hello_happy_codevarrior.js
+++ 8 kyu/hello_happy_codevarrior.js
@@ -32,3 +32,14 @@ Warrior.prototype.toString = function(){
 
 
 
+function Warrior(n){
+    let name = n;  
+    this.name = function(n){
+      if( n ) name=n;
+      return name;
+    }
+  }
+    
+  Warrior.prototype.toString = function(){
+      return "Hi! my name's "+this.name();
+  }
