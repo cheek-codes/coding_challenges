132570ffd0cdc579a88c64378dc48cb549735409
cheek-codes <cheek.codes@gmail.com>
2023-01-16 23:03:04 -0500

    swap values

---
+++ 8 kyu/swap_values.js
@@ -0,0 +1,68 @@
+// swap values
+
+// I would like to be able to pass an array with two elements to my swapValues function to swap the value. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+// function swapValues() {
+//     var args = Array.prototype.slice.call(arguments);
+//     var temp = args[0];
+//     args[0] = args[1];
+//     args[1] = temp;
+// }
+
+
+
+
+
+
+function swapValues() {
+    var args = arguments[0]
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}
+
+
+
+
+
+
+function swapValues() {
+    return arguments[0].reverse();
+}
+
+
+
+
+
+
+function swapValues(args) {
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}
+
+
+
+
+
+
+function swapValues(arr) {
+    return arr.reverse();
+}
+
+
+
+
+
+
+const swapValues = (arr) => [arr[0], arr[1]] = [arr[1], arr[0]]
+
+
+
+
+
+
+
+function swapValues(a){a.reverse()}
