fbbdf6faa137a6f21744fb6e9044c13ded7b02d2
cheek-codes <cheek.codes@gmail.com>
2023-01-17 20:27:36 -0500

    broken counter ans

--- 8 kyu/broken_counter.js
+++ 8 kyu/broken_counter.js
@@ -17,3 +17,94 @@
 //   Counter.prototype.reset() {
 //     this.value = 0;
 //   };
+
+
+
+
+
+
+function Counter() {
+    this.value = 0;
+  }
+  
+  Counter.prototype.increase= function(){
+    this.value++;
+  };
+  
+  Counter.prototype.getValue= function(){
+    return this.value;
+  };
+  
+  Counter.prototype.reset= function(){
+    this.value = 0;
+  };
+
+
+
+
+
+
+
+  class Counter {
+	constructor() {
+  	this.value = 0;
+  }
+  
+  increase() {
+    this.value++;
+  };
+
+  getValue() {
+    return this.value;
+  };
+
+  reset() {
+    this.value = 0;
+  };
+}
+
+
+
+
+
+
+function Counter() {
+    this.value = 0;
+  }
+  
+  Counter.prototype = {
+    increase : function(){this.value++;},
+    getValue : function(){ return this.value;},
+    reset : function(){this.value = 0;}
+  };
+
+
+
+
+
+
+class Counter {
+    constructor() { this.reset();      }
+    increase()    { this.value++;      }
+    getValue()    { return this.value; }
+    reset()       { this.value = 0;    }
+}
+
+
+
+
+
+
+function Counter() {
+    this.value = 0;
+    this.increase = () => this.value++;
+    this.getValue = () => this.value;
+    this.reset = () => this.value = 0;
+};
+
+
+
+
+
+
+this.value=0;function Counter(){};Counter.prototype.increase=()=>this.value++;Counter.prototype.getValue=()=>this.value;Counter.prototype.reset=()=>this.value=0//Only true madlads program in one line
