25fff96670976ab14ae787f8c039abc52bb270ce
cheek-codes <cheek.codes@gmail.com>
2023-01-19 20:41:42 -0500

    fixme

---
+++ 7 kyu/fixme_get_full_name.js
@@ -0,0 +1,28 @@
+// FIXME: Get full name
+
+// The code provided is supposed return a person' fullname given their first and last names.
+
+// But it's not working properly.
+
+// Notes:
+// The first and/or last names are never null, but may be empty.
+
+// Taks
+// Fix the bug so we can all go home early.
+
+// class Dinglemouse{
+
+//     constructor( firstName, lastName ){
+//     }
+    
+//     getFullName(){
+//       return firstName+" "+lastName
+//     }
+    
+// }
+
+
+
+
+
+
