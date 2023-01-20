d7ac98137fe074653086f62073221967a9ad40f4
cheek-codes <cheek.codes@gmail.com>
2023-01-19 20:53:13 -0500

    fixme ans

--- 7 kyu/fixme_get_full_name.js
+++ 7 kyu/fixme_get_full_name.js
@@ -26,3 +26,122 @@
 
 
 
+class Dinglemouse{
+
+    constructor( firstName, lastName ){
+      this.firstName = firstName
+      this.lastName = lastName
+    }
+    
+    getFullName(){
+      if (this.firstName === ""){
+        return this.lastName
+      }
+      else if(this.lastName == ""){
+        return this.firstName
+      }
+      else{
+        return `${this.firstName} ${this.lastName}`
+      }
+    }
+    
+}
+
+
+
+
+
+
+class Dinglemouse{
+
+    constructor( firstName, lastName ){
+      this._firstName=firstName;
+      this._lastName=lastName;
+    }
+    
+    getFullName(){
+      return `${this._firstName} ${this._lastName}`.trim();
+    }
+    
+}
+
+
+
+
+
+
+class Dinglemouse{
+    constructor( firstName, lastName ){
+      this.firstName = firstName.concat(" ");
+      this.lastName = lastName;
+    }
+    
+    getFullName(){
+      return this.firstName.concat(this.lastName).trim();
+    }
+    
+}
+
+
+
+
+
+
+function Dinglemouse(r, s) {
+    this.getFullName = _ => (r + ' ' + s).trim();
+}
+
+
+
+
+
+
+class Dinglemouse{
+  
+    constructor( firstName, lastName ){
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    getFullName(){
+      var full = this.firstName+" "+this.lastName;
+      return full.trim();
+    }
+    
+}
+
+
+
+
+
+
+class Dinglemouse {
+
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    getFullName() {
+      return `${this.firstName} ${this.lastName}`.trim();
+    }
+    
+}
+
+
+
+
+
+
+
+class Dinglemouse {
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    getFullName(){
+      const { firstName, lastName } = this;
+      return `${firstName} ${lastName}`.trim();
+    }
+}
