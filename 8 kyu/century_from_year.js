d5e0d803b203e958d506cc364e1e3ba0f1c0764a
cheek-codes <cheek.codes@gmail.com>
2023-01-21 22:12:20 -0500

    centur from year

---
+++ 8 kyu/century_from_year.js
@@ -0,0 +1,87 @@
+// century from year
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second centur spans form the year 101 up to and including the year 200, etc.
+
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+
+
+
+// Will we be given a string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers?
+
+
+
+
+// Return the century from the given year
+
+
+
+
+// 1. If we were given century(1705), should return 18
+// 2. If we were given century(1900), should return 19
+// 3. If we were given century(1601), should return 17
+// 4. If we were given century(2000), should return 20
+
+
+
+
+
+const century = year => Math.ceil(year/100)
+
+
+
+
+
+
+function century(year) {
+    return Math.ceil(year/100); //using ceiling method to round up to nearest century (100)
+}
+
+
+
+
+
+
+function century(year) {
+    return (year + 99) / 100 | 0;
+}
+
+
+
+
+
+
+function century(year) {
+    return Math.ceil(year/100);
+}
+
+
+
+
+
+
+const century = year => year % 100 === 0 ? parseInt(year / 100) : parseInt(year / 100) + 1;
+
+
+
+
+
+
+
+function century(year) {
+    var century = 0;
+    
+    for(var i = 0; i < year; i++) {
+      if(i % 100 == 0) {
+        century++;
+      }
+    }
+    return century;
+}
