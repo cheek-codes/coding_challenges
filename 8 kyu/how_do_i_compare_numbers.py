6b50e02e90bb3bdca35d8cb1ec052dc85fba231d
cheek-codes <cheek.codes@gmail.com>
2023-01-29 22:43:43 -0500

    how do i compare numbers

--- 8 kyu/how_do_i_compare_numbers.py
+++ 8 kyu/how_do_i_compare_numbers.py
@@ -1,17 +1,17 @@
-// How do I compare numbers?
+# How do I compare numbers?
 
-// What could be easier than comparing integer numbers? However, the given piece of code doesn't recognize some of the numbers for a reason to be found. Your task is to find the bug and eliminate it.
-
-
+# What could be easier than comparing integer numbers? However, the given piece of code doesn't recognize some of the numbers for a reason to be found. Your task is to find the bug and eliminate it.
 
 
-// def what_is(x):
-//     if x is 42:
-//         return 'everything'
-//     elif x is 42 * 42:
-//         return 'everything squared'
-//     else:
-//         return 'nothing'
+
+
+def what_is(x):
+    if x is 42:
+        return 'everything'
+    elif x is 42 * 42:
+        return 'everything squared'
+    else:
+        return 'nothing'
 
 
 
