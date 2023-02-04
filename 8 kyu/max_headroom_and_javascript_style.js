5d42ed65ab355b5dd268e967e61e1f2280fd124c
cheek-codes <cheek.codes@gmail.com>
2023-02-03 23:36:15 -0500

    max headroom and javascript style

---
+++ 8 kyu/max_headroom_and_javascript_style.js
@@ -0,0 +1,117 @@
+// max headroom and javascript style
+
+// shouldn't the two functions "getMax1" and "getMax2" be equivalent and return the same value? Can you spot a problem and fix it? Can you learn something about javascript style in this kata?
+
+
+
+
+// function getMax1()
+// {
+//   var max = 
+//   {
+//    name: 'Max Headroom'
+//   }
+//   return max;
+// }
+
+// function getMax2()
+// {
+//   return
+//   {
+//     name: 'Max Headroom'
+//   }
+// }
+
+
+
+
+
+
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+
+
+
+
+
+function getMax1()
+{
+  var max = {name: 'Max Headroom'}
+  return max;
+}
+
+function getMax2()
+{
+// Have your return object on same line
+return {name: 'Max Headroom'};
+}
+
+
+
+
+
+
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  // note Javascript does not require the semicolon but can assume it meaning nothing is returned
+  // instead move the brace up
+  return  {
+    name: 'Max Headroom'
+  }
+}
+
+
+
+
+
+
+function getMax1() {
+    var max = {
+     name: 'Max Headroom'
+    }
+    return max;
+  }
+  
+  function getMax2() {
+    return {
+      name: 'Max Headroom'
+    }
+}
+
+
+
+
+
+
+const getMax1 = () => {
+    const max = {name: `Max Headroom`};
+    return max;
+  };
+  
+  const getMax2 = () =>
+    ({name: `Max Headroom`});
