49fd1ac6ab5dd4cd90c2c1a065468ea2beadf4f0
cheek-codes <cheek.codes@gmail.com>
2023-02-04 00:10:06 -0500

    debug sum of digits of a number ans

--- 7 kyu/debug_sum_of_digits_of_a_number.js
+++ 7 kyu/debug_sum_of_digits_of_a_number.js
@@ -21,3 +21,39 @@
 
 
 
+function getSumOfDigits(integer) {
+    return integer.toString().split("").reduce((a, c) => a + Number(c), 0)
+}
+
+
+
+
+
+
+function getSumOfDigits(integer) {
+    return (integer+'').split('').reduce((sum, d) => sum + (+d || 0), 0);
+}
+
+
+
+
+
+
+function getSumOfDigits(integer) {
+  
+    return Array.from(String(integer), Number).reduce((a,b) => a + b)
+}
+
+
+
+
+
+
+function getSumOfDigits(integer) {
+    var sum = null;
+    var digits =  Math.floor(integer).toString();
+    for(var ix = 0; ix < digits.length; ix++) {
+      sum += +digits[ix];
+    }
+    return sum;
+}
