a3fce700816c6eddb36ad646ce8892230d1529c8
cheek-codes <cheek.codes@gmail.com>
2023-02-04 23:59:50 -0500

    debug sum of digits of a number

---
+++ 7 kyu/debug_sum_of_digits_of_a_number.js
@@ -0,0 +1,23 @@
+// debug sum of digits of a number
+
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits. Assume that argument is an integer.
+
+// Example
+// 123  => 6
+// 223  => 7
+// 1337 => 14
+
+// function getSumOfDigits(integer) {
+//     var sum = null;
+//     var digits =  Math.floor(integer).toString();
+//     for(var ix = 1; ix < digits.length; ix = sum + 1) {
+//       sum =+ digits[ix + 1]);
+//     }
+//     return sum;
+//   }
+
+
+
+
+
+
