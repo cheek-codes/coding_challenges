9053511989fa99c67b7760682fb3cd56a06964d6
cheek-codes <cheek.codes@gmail.com>
2023-02-05 21:26:50 -0500

    debug the functions easy ans

--- 7 kyu/debug_the_functions_easy.js
+++ 7 kyu/debug_the_functions_easy.js
@@ -48,3 +48,37 @@ return arr.reduce((n, x) => n + x, 0)
 function reverse(str) {
 return [...str].reverse().join("")
 }
+
+
+
+
+
+
+function multi(arr) {
+    var answer = 1;
+    for(var i = 0; i < arr.length; i++){
+        answer *= arr[i];  
+}
+return answer;
+}
+function add(arr) {
+var answer = 0;
+for(var i = 0; i<arr.length; i++){
+    answer += arr[i];
+}
+return answer;
+}
+function reverse(str) {
+return str.split("").reverse().join("");
+}
+
+
+
+
+
+
+const multi = arr => arr.reduce((a,b)=> a*b);
+
+const add = arr=> arr.reduce((a,b)=> a+b);
+
+const reverse = str => str.split('').reverse().join('');
