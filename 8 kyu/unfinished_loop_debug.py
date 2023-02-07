fb3e0b320b532145a8e4c1e235c14802e48c5a97
cheek-codes <cheek.codes@gmail.com>
2023-02-06 22:39:56 -0500

    unfinished loop bug fixing ans5

--- 8 kyu/unfinished_loop_debug.py
+++ 8 kyu/unfinished_loop_debug.py
@@ -56,3 +56,17 @@ def create_array(n):
 
 
 create_array = lambda n: list(range(1,n+1))
+
+
+
+
+
+
+
+def create_array(n):
+    res = []
+    i = 1
+    while i <= n:
+        res.append(i)
+        i += 1
+    return res
