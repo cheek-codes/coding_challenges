117835965fd82d9bd5badb3749b286eb9ba0a879
cheek-codes <cheek.codes@gmail.com>
2023-02-08 21:12:10 -0500

    debug the functions easy

---
+++ 7 kyu/debug_the_functions_easy.js
@@ -0,0 +1,21 @@
+// debug the functions easy
+
+// should be easy, begiin by looking at the code. debug the code and the functions should work.
+
+// there are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
+
+// function multi(arr) {
+//   return arr * arr;
+// }
+// function add(arr) {
+//   return arr + arr;
+// }
+// function reverse(str) {
+//   return str.reverse();
+// }
+
+
+
+
+
+
