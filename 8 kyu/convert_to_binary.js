d7d88a1de8740489736b07aa79882f791678612f
cheek-codes <cheek.codes@gmail.com>
2023-02-18 22:47:14 -0500

    convert to binary

---
+++ 8 kyu/convert_to_binary.js
@@ -0,0 +1,75 @@
+// convert to binary
+
+// Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+// to_binary(1)  /* should return 1 */
+// to_binary(5)  /* should return 101 */
+// to_binary(11) /* should return 1011 */
+// Example:
+
+// toBinary(1)  /* should return 1 */
+// toBinary(5)  /* should return 101 */
+// toBinary(11) /* should return 1011 */
+
+
+
+
+// Will we be given a string? Will we be given special characters? Will we be given an array? Will we be given floats? Will we be given integers?Will we be given a negative number? Will we be given positive numbers?
+
+
+
+
+// Return a binary of the given number
+
+
+
+
+// If we were given toBinary(1), should return 1
+// If we were given toBinary(5), should return 101
+// If we were given toBinary(11), should return 1011
+
+
+
+
+
+
+toBinary = n => + n.toString(2)
+
+
+
+
+
+
+const toBinary = n => + n.toString(2)
+
+
+
+
+
+
+function toBinary(n){
+    return +n.toString(2)
+}
+
+
+
+
+
+
+toBinary = (n) => parseInt(n.toString(2));
+
+
+
+
+
+
+const toBinary = n => Number(n.toString(2))
+
+
+
+
+
+
+function toBinary(n){
+    return parseInt(n.toString(2));
+}
