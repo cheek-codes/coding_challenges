a8b48ac3b03dadc8640218c579f1e32aa7b1b95d
cheek-codes <cheek.codes@gmail.com>
2023-04-05 01:49:43 -0400

    measuring average speed

---
+++ 7 kyu/measuring_average_speed.js
@@ -0,0 +1,33 @@
+// Measuring Average Speed
+
+// Speed is a crucial measure of success for any aspiring warrior, so let's write a function to calculate it.
+
+// Average Speed is calculated by dividing distance by time. Given two strings as input the first of which indicates a codewarrior's distance travelled (in metres or kilometres) and the second indicating the time it takes them to travel (in seconds or minutes), return a string indicating their speed in miles per hour rounded to the nearest integer.
+
+// For the purposes of this kata one metre per second is defined as 2.23694 miles per hour.
+
+// So for example given the input values of distance & time "3km" and "5min" we should return a speed value of "22mph".
+
+
+
+
+// Will we be given strings? Will we be given an empty string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+
+// Return a new string of a speed in miles per hour
+
+
+
+
+// If we were given calculateSpeed("100m", "10s"),should return "22mph"
+// If we were given calculateSpeed("3km", "5min"), should return "22mph"
+// If we were given calculateSpeed("35m", "293s"), should return "0mph"
+// If we were given calculateSpeed("573km", "39min"), should return "548mph"
+
+
+
+
+
+
