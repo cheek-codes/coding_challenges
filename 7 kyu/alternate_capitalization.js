c3b0e4da7f71bd7e067660415c3604ac7983c1c8
cheek-codes <cheek.codes@gmail.com>
2023-04-07 00:50:36 -0400

    alternate capitalization

---
+++ 7 kyu/alternate_capitalization.js
@@ -0,0 +1,27 @@
+// alternate capitalization
+
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+
+
+
+// Will we be given a string? will we be given an empty string? Will we be given special characters? Will we be given floats? will we be given integers?
+
+
+
+
+// Return a new string where it alternates capitalizing letters in two different ways
+
+
+
+
+// If we were given capitalize("abcdef"), should return ['AbCdEf', 'aBcDeF']
+// If we were given capitalize("codewars"), should return ['CoDeWaRs', 'cOdEwArS']
+// If we were given capitalize("abracadabra"), should return ['AbRaCaDaBrA', 'aBrAcAdAbRa']
+// If we were given capitalize("codewarriors"), should return ['CoDeWaRrIoRs', 'cOdEwArRiOrS']
