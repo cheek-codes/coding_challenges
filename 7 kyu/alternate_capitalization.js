54e9be9be49c5f193c97f34caa9e3905b5e6fe02
cheek-codes <cheek.codes@gmail.com>
2023-04-07 00:52:23 -0400

    alternate capitalization ans

--- 7 kyu/alternate_capitalization.js
+++ 7 kyu/alternate_capitalization.js
@@ -25,3 +25,88 @@
 // If we were given capitalize("codewars"), should return ['CoDeWaRs', 'cOdEwArS']
 // If we were given capitalize("abracadabra"), should return ['AbRaCaDaBrA', 'aBrAcAdAbRa']
 // If we were given capitalize("codewarriors"), should return ['CoDeWaRrIoRs', 'cOdEwArRiOrS']
+
+
+
+
+
+
+function capitalize(str) {
+    let even = ""
+    let odd = ""
+    for (let i = 0; i < str.length; i++) {
+      if (i % 2 === 0) {
+        even += str[i].toUpperCase()
+        odd += str[i]
+      } else {
+        even += str[i]
+        odd += str[i].toUpperCase()
+      }
+    }
+    return [even, odd]
+}
+
+
+
+
+
+
+function capitalize(s){
+    const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+    const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+    return [even, odd];
+};
+
+
+
+
+
+
+function capitalize(s){
+    return [0,1].map(r=>[...s].map((c,i)=>i%2===r?c.toUpperCase():c).join(''));
+};
+
+
+
+
+
+
+function capitalize(s){
+    return [[...s].map((x,i) => i % 2 == 0 ? x.toUpperCase() : x).join(''),
+            [...s].map((x,i) => i % 2 != 0 ? x.toUpperCase() : x).join('')]
+}
+
+
+
+
+
+
+function capitalize(s) {
+    return s.split('').reduce((acc, val, i) => {
+        acc[0] += i % 2 === 0 ? val.toUpperCase() : val.toLowerCase();
+        acc[1] += i % 2 === 0 ? val.toLowerCase() : val.toUpperCase();
+        return acc;
+    }, ['', '']);
+};
+
+
+
+
+
+
+const capitalize = s => {
+    const arr = s.split('');
+    let a = '', b = '';
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 2) {
+        a += arr[i];
+        b += arr[i].toUpperCase();
+      } else {
+        a += arr[i].toUpperCase();
+        b += arr[i];
+      }
+    }
+    
+    return [a, b];
+};
