190ae7d8f8e755fc65bad98360ae116fa3a82e2e
cheek-codes <cheek.codes@gmail.com>
2023-04-09 22:48:49 -0400

    calculate price excluding vat

---
+++ 8 kyu/calculate_price_excluding_vat.js
@@ -0,0 +1,38 @@
+// Calculate Price Excluding VAT
+
+// Write a function that calculates the original product price, without VAT.
+
+// Example
+// If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+// Thus, if your function receives 230.00 as input, it should return 200.00
+
+// Notes:
+
+// VAT is always 15% for the purposes of this Kata.
+// Round the result to 2 decimal places.
+// If null value given then return -1
+
+
+
+
+// Will we be given a string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+
+// Return -1 if excludingVatPrice(price) is null
+// Return a number rounded to the nearest two decimal places after mathing price/1.15
+
+
+
+
+// If we were given excludingVatPrice(230), should return 200.00
+// If we were given excludingVatPrice(123), should return  106.96
+// If we were given excludingVatPrice(null), should return -1
+
+
+
+
+
+
