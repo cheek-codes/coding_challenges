045fcbba3e6e2b3fd01f0349746c5098d8f68b22
cheek-codes <cheek.codes@gmail.com>
2023-04-10 23:22:20 -0400

    will you make it

---
+++ 8 kyu/will_you_make_it.js
@@ -0,0 +1,24 @@
+// will you make it?
+
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+
+
+
+// Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? will we be given negative numbers? Will we be given positive number?
+
+
+
+
+// Return true if mpg*fl is greater than or equal to 0
+// Return false if mpg*fl is less than 0
+
+
+
+
+// If we were given zeroFuel(50, 25, 2), should return true
+// If we were given zeroFuel(100, 50, 1), should return false
