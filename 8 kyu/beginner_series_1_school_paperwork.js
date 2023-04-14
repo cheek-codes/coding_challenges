6a054f3844ca4720a300e9edca9e8e8353aa197b
cheek-codes <cheek.codes@gmail.com>
2023-04-13 23:07:30 -0400

    Beginner Series #1 School Paperwork

--- 8 kyu/beginner_series_1_school_paperwork.js
+++ 8 kyu/beginner_series_1_school_paperwork.js
@@ -37,3 +37,58 @@
 
 
 
+function paperwork(n, m) {
+    if(n<=0 || m <=0){
+      return 0
+    }
+    else{
+      return n*m
+    }
+}
+
+
+
+
+
+
+const paperwork = (n, m) => n <= 0 || m <= 0 ? 0 : n*m
+
+
+
+
+
+
+function paperwork(n, m) {
+    return n > 0 && m > 0 ? n * m : 0
+}
+
+
+
+
+
+
+function paperwork(n, m) {
+    return n < 0 || m < 0 ? 0 : n * m;
+}
+
+
+
+
+
+
+paperwork = (n, m) => n < 0 || m < 0 ? 0 : n * m
+
+
+
+
+
+
+const paperwork = (n, m) => Math.max(0, n) * Math.max(0, m);
+
+
+
+
+
+
+
+paperwork = (n, m) => n > 0 && m > 0 ? n * m : 0;
