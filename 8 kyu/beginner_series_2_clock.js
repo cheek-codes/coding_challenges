e59864da925a072ea48ac387d6de0120e0825d33
cheek-codes <cheek.codes@gmail.com>
2023-04-13 22:22:49 -0400

    Beginner Series #2 Clock

---
+++ 8 kyu/beginner_series_2_clock.js
@@ -0,0 +1,22 @@
+// Beginner Series #2 Clock
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+
+
+
+// Will we be given a string? Will we be given any special characters? will we be given a float? Will we be given an integer? Will we be given a negative number? Will we be given a positive number? Will we be given any hours greater than 23? Will we be given any minutes greater than 59? Will we be given any seconds greater than 59?
