87bd43a51e5d2b94a81d07d6178d135df4554a24
cheek-codes <cheek.codes@gmail.com>
2023-05-10 23:10:50 -0400

    unexpected parsing

---
+++ 8 kyu/unexpected_parsing.py
@@ -0,0 +1,13 @@
+# Unexpected parsing
+
+# here is a piece of code:
+
+# def get_status(is_busy):
+#     status = "busy" if is_busy else "available"
+#     return status
+
+# expected behavior
+
+# Function should return a dictionary/object/hash with "status" as a key, whose value can: "busy" or "available" depending on the truth value of the argument "is_busy".
+
+# But as you will see after clicking "run" or "attempt" this code has several bugs, please fix them.
