47440b33b74189afc6d21c8989b56e3bd68c6f98
cheek-codes <cheek.codes@gmail.com>
2023-06-08 22:53:36 -0400

    incorrect division method ans1

--- 8 kyu/incorrect_division_method.py
+++ 8 kyu/incorrect_division_method.py
@@ -3,3 +3,11 @@
 # This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it
 
 # const solve = (x, y) => x // y
+
+
+
+
+
+
+def divide_numbers(x,y):
+    return x / y
