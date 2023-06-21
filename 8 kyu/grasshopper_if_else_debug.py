b327bc89f234011e6874cb04b0f15b920654d444
cheek-codes <cheek.codes@gmail.com>
2023-06-20 22:45:09 -0400

    grasshopper if/else syntax debug ans2

--- 8 kyu/grasshopper_if_else_debug.py
+++ 8 kyu/grasshopper_if_else_debug.py
@@ -50,3 +50,6 @@ def check_alive(health):
 
 
 
+def check_alive(health: int) -> bool:
+    """ Return `true` if the player's health is greater than 0 or `false` if it is 0 or below. """
+    return health > 0
