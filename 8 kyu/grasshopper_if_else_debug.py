40e0b84116c6bbc421440765321a9f8c9e21d45b
cheek-codes <cheek.codes@gmail.com>
2023-06-24 22:40:24 -0400

    grasshopper if/else syntax debug

--- 8 kyu/grasshopper_if_else_debug.py
+++ 8 kyu/grasshopper_if_else_debug.py
@@ -29,3 +29,7 @@
 
 
 
+# E: 1. If we are given -9, should return false
+# E: 2. If we are given 0, should return false
+# E: 3. If we are given 2, should return true
+# E: 4. If we are given 9, should return true
