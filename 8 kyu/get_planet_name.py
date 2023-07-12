69210c0593bf57a12e5f456d6b2ff99c5764d1e4
cheek-codes <cheek.codes@gmail.com>
2023-07-11 22:31:20 -0400

    get planet name ans2

--- 8 kyu/get_planet_name.py
+++ 8 kyu/get_planet_name.py
@@ -43,3 +43,20 @@ def get_planet_name(id):
 
 
 
+def get_planet_name(id):
+    return {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus",
+        8: "Neptune",
+    }.get(id, None)
+
+
+
+
+
+
