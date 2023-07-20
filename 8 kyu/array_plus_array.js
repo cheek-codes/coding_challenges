5328455179a816c7dca7ff7b8043bf34c5136358
cheek-codes <cheek.codes@gmail.com>
2023-07-19 21:43:50 -0400

    array plus array

--- 8 kyu/array_plus_array.js
+++ 8 kyu/array_plus_array.js
@@ -40,38 +40,3 @@ function arrayPlusArray(arr1, arr2){
     }
     return sum
 }
-
-
-
-function array(arr1, arr2){
-    return arr1.concat(arr2).reduce((acc, cur) => acc + cur)
-}
-
-
-
-function arrayPlusArray(arr1, arr2){
-    let arr = [...arr1, ...arr2]
-    return arr.reduce((a, b) => a + b)
-}
-
-
-
-function arrayPlusArray(...arrays){
-    return [].concat(...arrays).reduce((a, b)=> a +b , 0)
-}
-
-
-
-const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((a, b) => a + b, 0)
-
-
-
-function arrayPlusArray(arr1, arr2){
-    return arr1.reduce((sum, cur) => sum + cur) + arr2.reduce((sum, cur) => sum + cur)
-}
-
-
-
-function arrayPlusArray(arr1, arr2){
-    return[...arr1, ...arr2].reduce((a, b) => a + b, 0)
-}
