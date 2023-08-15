698f4ee4d347c83a5ef6b74865fc9d371dcc6e44
cheek-codes <cheek.codes@gmail.com>
2023-08-14 22:02:10 -0400

    return negative

--- 8 kyu/return_negative.py
+++ 8 kyu/return_negative.py
@@ -11,3 +11,8 @@
 # Notes:
 # The number can be negative already, in which case no change is required.
 # Zero(0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+
+
+
+# P: Will we be given strings? Will we be given empty strings? Will we be given floats? Will we be given positive integers? Will we be given negative integers?
