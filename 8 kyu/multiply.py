026c43146c64e879fa71859c6e1cfa09ee99964c
cheek-codes <cheek.codes@gmail.com>
2023-08-17 21:56:38 -0400

    multiply

--- 8 kyu/multiply.py
+++ 8 kyu/multiply.py
@@ -9,3 +9,8 @@
 
 
 # P: Will we be given a string? Will we be given an empty string? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+
+# R: Return a code that fixes it because this code does not work
