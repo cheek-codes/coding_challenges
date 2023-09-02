5ec5961be6bc75d7b6ebb58f41460b69e204f37a
cheek-codes <cheek.codes@gmail.com>
2023-09-01 21:45:04 -0400

    jenny secret message

--- 8 kyu/jenny_secret_message.py
+++ 8 kyu/jenny_secret_message.py
@@ -8,3 +8,8 @@
 #     return "Hello, {name}!".format(name=name)
 #     if name == "Johnny":
 #         return "Hello, my love!"
+
+
+
+
+# P: Will we be given floats? Will we be given integers? Will we be given strings? Will we be given empty strings? Will we be given special characters? Will we be given uppercase characters? Will we be given lowercase characters?
