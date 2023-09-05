125c81cfe5b3aaabc749ca5214ce872390ac3a20
cheek-codes <cheek.codes@gmail.com>
2023-09-05 15:24:59 -0400

    even or odd ans 3

--- 8 kyu/even_or_odd.py
+++ 8 kyu/even_or_odd.py
@@ -31,3 +31,19 @@ def even_or_odd(number):
         return "Even"
     else:
         return "Odd"
+    
+
+
+
+
+
+def even_or_odd(number):
+	return 'Odd' if number % 2 else 'Even'
+
+
+
+
+
+
+def even_or_odd(number):
+  return 'Even' if number % 2 == 0 else 'Odd'
