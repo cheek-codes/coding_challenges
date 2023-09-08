a7222444e380d80cff44e988d4f62fbf013ebe56
cheek-codes <cheek.codes@gmail.com>
2023-09-08 15:22:35 -0400

    even or odd example

--- 8 kyu/even_or_odd.py
+++ 8 kyu/even_or_odd.py
@@ -6,3 +6,9 @@
 
 # R: Return "Even" if the given integer is an even number (if the given number is divisible by 2 and has no remainders)
 # R: Return "Odd" if the given integer is an odd number (if the given number is not divisible by 2 and has remainders)
+
+# E: 1. If we were given 2, should return "Even"
+# E: 2. If we were given 7, should return "Odd"
+# E: 3. If we were given -42, should return "Even"
+# E: 4. If we were given -7, should return "Odd"
+# E: 5. If we were given 0, should return "Even"
