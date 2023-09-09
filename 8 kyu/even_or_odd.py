28ef008aae4711faa3455f4a6d9123721d4191c3
cheek-codes <cheek.codes@gmail.com>
2023-09-09 15:19:46 -0400

    even or return

--- 8 kyu/even_or_odd.py
+++ 8 kyu/even_or_odd.py
@@ -3,3 +3,6 @@
 # Create a function that takes an integer as an argument and returns "Even" for even numbers of "Odd" for odd numbers.
 
 # Will we be given an empty string? Will we be given a string? Will we be given any special characters? Will we be given a float? Will we be given an integer? Will we be given a negative number? Will we be given a positive number?
+
+# R: Return "Even" if the given integer is an even number (if the given number is divisible by 2 and has no remainders)
+# R: Return "Odd" if the given integer is an odd number (if the given number is not divisible by 2 and has remainders)
