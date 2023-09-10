a07693c5b7d84b88a351d20f00c0c7429a6d25c8
cheek-codes <cheek.codes@gmail.com>
2023-09-10 15:16:42 -0400

    even or odd pypara

--- 8 kyu/even_or_odd.py
+++ 8 kyu/even_or_odd.py
@@ -1,3 +1,5 @@
 # Even or odd
 
 # Create a function that takes an integer as an argument and returns "Even" for even numbers of "Odd" for odd numbers.
+
+# Will we be given an empty string? Will we be given a string? Will we be given any special characters? Will we be given a float? Will we be given an integer? Will we be given a negative number? Will we be given a positive number?
