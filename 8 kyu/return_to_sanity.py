f2592f9fbc150acd81b9b821844304b0acf45a0b
cheek-codes <cheek.codes@gmail.com>
2023-09-11 13:08:24 -0400

    return to sanity

--- 8 kyu/return_to_sanity.py
+++ 8 kyu/return_to_sanity.py
@@ -2,3 +2,12 @@
 
 # This function should return an object, but it's not doing what's intended. What's wrong?
 
+# def mystery():
+#     results = {
+#     'sanity': 'Hello'}
+#     return 
+#     results
+
+
+
+
