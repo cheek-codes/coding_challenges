7ffa6a4ecf7ae26af8cd1ba6fcb097124f7e28b9
cheek-codes <cheek.codes@gmail.com>
2023-09-23 22:54:47 -0400

    opposite number r

--- 8 kyu/opposite_number.py
+++ 8 kyu/opposite_number.py
@@ -10,3 +10,10 @@
 
 
 # P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive numbers?
+
+
+
+
+# R: Return a new integer or float that is the opposite sign of the given parameter.
+
+
