f117cc92e90948915006541d8d49dbea4b70560a
cheek-codes <cheek.codes@gmail.com>
2023-09-25 23:36:32 -0400

    you cant code under pressure

---
+++ 8 kyu/you_cant_code_under_pressure.py
@@ -0,0 +1,18 @@
+# You can't code under pressure
+
+# Code as fast as you can ! you need to double the integer and return it
+
+
+
+
+# P: Will I be given integers? Will I be given floats? Will I be given strings? Will I be given empty strings? Will I be given special characters?
+
+
+
+# R: Return a new integer that has been doubled.
+
+
+
+# E: 1. If we were given 2, should return 4
+# E: 2. If we were given -2, should return 4
+# E: 3. If we were given 10, should return 20
