ef4491ecfca009d2d21c795b9b6b4289c4b3e2a3
cheek-codes <cheek.codes@gmail.com>
2023-09-27 22:48:08 -0400

    sum of positive

--- 8 kyu/sum_of_positive.py
+++ 8 kyu/sum_of_positive.py
@@ -20,3 +20,15 @@
 # E: 1. If we are given the array [1,-4,7,12], should return 20 because 1 + 7 + 12 = 20
 # E: 2. If we are given the array [2,4,6,0], should return 12 because 2 + 4 + 6 + 0 = 12
 # E: 3. If we are given the array [-9393,5,2,3], should return 10 because 5 + 2 + 3 = 10 
+
+
+
+
+
+
+def positive_sum(arr):
+    total_sum = 0
+    for num in arr:
+        if num > 0:
+            total_sum += num
+    return total_sum
