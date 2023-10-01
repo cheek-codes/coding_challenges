3139a8d04fe41852dd2e2096b2e4792e76bb5725
cheek-codes <cheek.codes@gmail.com>
2023-09-30 22:50:17 -0400

    fixme replace all dots

--- 8 kyu/fixme_replace_all_dots.py
+++ 8 kyu/fixme_replace_all_dots.py
@@ -30,3 +30,93 @@ var replaceDots = function(str) {
 # E: 2. If we are given "no dots", should return "no dots"
 # E: 3. If we are given "201.398.0281", should return "201-398-0281"
 # E: 4. If we are given "...", should return "---"
+
+
+
+
+
+
+def replace_dots(s):
+    return s.replace(".", "-")
+
+
+
+
+
+
+import re
+def replace_dots(str):
+    return re.sub(r"\.", "-", str)
+    # literally I had to only add a \ infront of the dot.
+    # this challenge is super hard for noobs and super easy for pros. it's not fair.
+
+
+
+
+
+
+import re
+def replace_dots(s):
+    return s.replace(".", "-")
+
+
+
+
+
+
+replace_dots = lambda x: x.replace(".","-")
+
+
+
+
+
+
+import re
+def replace_dots(str):
+    return re.sub(r"\.", "-", str, flags=re.MULTILINE)
+
+
+
+
+
+
+import re
+
+def replace_dots(s):
+    return re.sub("[.]", "-", s)
+
+
+
+
+
+def replace_dots(str):
+    if "." in str:
+        return str.replace(".","-")
+    elif str == "no dots":
+        return "no dots"
+    else:
+        return ""
+
+
+
+
+
+
+
+def replace_dots( _s ): return _s.replace('.', '-')
+
+
+
+
+
+
+import re
+def replace_dots(str):
+    return re.sub(r"[.]", "-", str)
+
+
+
+
+
+
+replace_dots=lambda _:_.replace('.','-')
