029dd95b5c5c93d273dd6713fae4f6b2121c2242
cheek-codes <cheek.codes@gmail.com>
2023-10-01 22:21:40 -0400

    is this my tail

--- 8 kyu/is_this_my_tail.py
+++ 8 kyu/is_this_my_tail.py
@@ -59,3 +59,31 @@ def correct_tail(body, tail):
 
 def correct_tail(body, tail):
     return body.endswith(tail)
+
+
+
+
+
+
+def correct_tail(body, tail):
+    return body[-1] == tail
+
+
+
+
+
+
+def correct_tail(body, tail):
+    sub = body[len(body)-len(tail)]
+    if sub == tail:
+        return True
+    else:
+        return False
+    
+
+
+
+
+
+def correct_tail(body, tail):
+    return True if body[-1]==tail else False
