db30443f755d8ce7baa0cd90fb2cf95fa41c25c0
cheek-codes <cheek.codes@gmail.com>
2023-10-05 19:37:42 -0400

    grasshopper debug

--- 8 kyu/grasshopper_debug.py
+++ 8 kyu/grasshopper_debug.py
@@ -113,3 +113,15 @@ def weather_info(fahrenheit):
 
 
 
+def weather_info (temp):
+    c = convertToCelsius(temp)
+    return '{} {}'.format(c, 'is freezing temperature' if c < 0 else 'is above freezing temperature')
+    
+def convertToCelsius (temp):
+  return (temp - 32) * 5 / 9.0
+
+
+
+
+
+
