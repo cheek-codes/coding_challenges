515d7822219dc0d7a44be2a1097671a624865989
cheek-codes <cheek.codes@gmail.com>
2023-10-06 00:28:48 -0400

    plant

--- 8 kyu/fix_your_code_before_the_garden_dies.py
+++ 8 kyu/fix_your_code_before_the_garden_dies.py
@@ -32,3 +32,54 @@ def rain_amount(rain_amount):
     else:
         return f"You need to give your plant {40-rain_amount}mm of water"
          
+
+
+
+
+
+def rain_amount(mm):
+    if mm < 40:
+        return "You need to give your plant " + str(40 - mm) + "mm of water"
+    else:
+        return "Your plant has had more than enough water for today!"
+    
+
+
+
+
+
+def rain_amount(mm):
+    if mm < 40:
+        return 'You need to give your plant {}mm of water'.format(40 - mm)
+    return 'Your plant has had more than enough water for today!'
+
+
+
+
+
+
+def rain_amount(mm):
+    if (mm < 40):
+         return f"You need to give your plant {40-mm}mm of water"
+    else:
+         return "Your plant has had more than enough water for today!"
+    
+
+
+
+
+
+def rain_amount(rain_amount):
+    if rain_amount < 40:
+        return "You need to give your plant " + str(40 - rain_amount) + "mm of water"
+    else:
+        return "Your plant has had more than enough water for today!"
+    
+
+
+
+
+
+def rain_amount(mm):
+    if mm >= 40: return 'Your plant has had more than enough water for today!'
+    else: return f'You need to give your plant {40-mm}mm of water'
