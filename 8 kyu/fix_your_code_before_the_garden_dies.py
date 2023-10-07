8a46b71a233116e85a6fadaf792d104c2fa80bf0
cheek-codes <cheek.codes@gmail.com>
2023-10-07 00:11:42 -0400

    fix your code before the garden dies

---
+++ 8 kyu/fix_your_code_before_the_garden_dies.py
@@ -0,0 +1,22 @@
+# Fix your code before the garden dies!
+
+# You have an award-winning garden and every day the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs.
+
+# Your task is to debug the code before your plants die!
+
+ 
+
+
+"""function rainAmount(mm){
+    if (rainAmount = 40) {
+         return "You need to give your plant " + {rainAmount - 40} + " mm of water"
+    };
+    if else {
+         return "Your plant has had more than enough water for today!"
+    };
+}"""
+
+
+
+
+# P: Will we be given empty strings? Will we be given strings? Will we be given special characters? Will we be given arrays? Will we be given floats? Will we be given integers? Will we be given negative numbers? Will we be given positive number?
