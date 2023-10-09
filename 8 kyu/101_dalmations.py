bf65f6b4a67da740798cdee458880650b2680752
cheek-codes <cheek.codes@gmail.com>
2023-10-08 23:59:33 -0400

    101 dalmations

--- 8 kyu/101_dalmations.py
+++ 8 kyu/101_dalmations.py
@@ -19,3 +19,52 @@
   
 return respond
 """
+
+
+
+
+
+
+def how_many_dalmatians(n):
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
+    if n <= 10:
+        return dogs[0]
+    elif n <= 50:
+        return dogs[1]
+    elif n == 101: 
+          return dogs[3]
+    else:
+        return dogs[2]
+    
+
+
+
+
+
+def how_many_dalmatians(n):
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
+    return dogs[0] if n <= 10 else dogs[1] if n <=50 else dogs[3] if n == 101 else dogs[2]
+
+
+
+
+
+
+def how_many_dalmatians(n):
+    return "Hardly any" if n <= 10 else("More than a handful!" if n <= 50 else("101 DALMATIONS!!!"if n == 101 else "Woah that's a lot of dogs!"))
+
+
+
+
+
+
+how_many_dalmatians=lambda n: ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"][sum([n>10, n>50, n>=101])]
+
+
+
+
+
+
+def how_many_dalmatians(n):
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!","101 DALMATIONS!!!"];
+    return dogs[(n>10)+(n>50)+(n==101)+(n>101)]
