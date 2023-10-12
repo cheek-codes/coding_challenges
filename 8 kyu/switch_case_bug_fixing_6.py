ef8969d5a95f10f665b0ac3f29db4138236187d7
cheek-codes <cheek.codes@gmail.com>
2023-10-12 17:55:51 -0400

    switch case bug

---
+++ 8 kyu/switch_case_bug_fixing_6.py
@@ -0,0 +1,27 @@
+# Switch/Case - Bug Fixing #6
+
+# oh no! timmy's evalObject function doesn't work. He uses Swithc/Cases to evaluate the given properties of an objec, can you fix timmy's function?
+
+"""def eval_object(v):
+    match "operation":
+        case "+":
+            return v["a"] + v["b"]
+        case "-":
+            return v["a"] - v["b"]
+        case "/":
+            return v["a"] / v["b"]
+        case "*":
+            return v["a"] * v["b"]
+        case "%":
+            return v["a"] % v["b"]
+        case "**":
+            return v["a"] ** v["b"]
+        case _:
+            return 1
+"""
+
+
+
+
+
+
