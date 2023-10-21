041d2873c4e3380c1b99f761fca1b9d48898a47a
cheek-codes <cheek.codes@gmail.com>
2023-10-21 18:55:29 -0400

    coefficients

--- 8 kyu/coefficients_of_the_quadratic_equation.js
+++ 8 kyu/coefficients_of_the_quadratic_equation.js
@@ -43,15 +43,3 @@ function quadratic(x1, x2){
     let c = x1 * x2
     return [1, -b, c]
 }
-
-
-
-
-function quadratic(x1, x2){
-    return [1, -(x1 + x2), (x1 * x2)]
-}
-
-
-
-
-const quadratic = (x1, x2) => [1, -(x1 + x2), (x1 * x2)]
