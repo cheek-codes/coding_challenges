3d5ffa951ca15bb1f24fccf4b1b95e62c8387d9c
cheek-codes <cheek.codes@gmail.com>
2023-10-25 00:18:40 -0400

    convert bool yes

--- 8 kyu/convert_bool_to_string_yes_or_no.js
+++ 8 kyu/convert_bool_to_string_yes_or_no.js
@@ -31,21 +31,3 @@ function boolToWord(bool){
         return "No"
     }
 }
-
-
-
-// less lines
-function boolToWord(bool){
-    // set up if else statements
-    // if bool is === true, return as yes  
-    // or else return "No" in place of false
-    return bool ? "Yes" : "No"
-}
-
-
-
-// even less lines
-    // set up if else statements
-    // if bool is === true, return as yes  
-    // or else return "No" in place of false
-    const boolToWord = bool => bool ? "Yes" : "No"
